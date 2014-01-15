a33477ca3284998405a1a47e11e068d95333842d
Jonathan Tsai <jontsai@jonathantsai.com>
2014-01-15 05:59:16 +0000

    make email template a configurable setting

--- apps/prelaunch/emails.py
+++ apps/prelaunch/emails.py
@@ -3,13 +3,15 @@ from htk.mailers import send_email
 from htk.utils import htk_setting
 
 def prelaunch_email(prelaunch_signup):
+    template = htk_setting('HTK_PRELAUNCH_EMAIL_TEMPLATE', HTK_PRELAUNCH_EMAIL_TEMPLATE)
     subject = htk_setting('HTK_PRELAUNCH_EMAIL_SUBJECT', HTK_PRELAUNCH_EMAIL_SUBJECT)
     bcc = htk_setting('HTK_PRELAUNCH_EMAIL_BCC', HTK_PRELAUNCH_EMAIL_BCC)
+
     context = {
         'prelaunch_signup': prelaunch_signup,
     }
     send_email(
-        template='prelaunch',
+        template=template,
         subject=subject,
         to=[prelaunch_signup.email,],
         bcc=bcc,
