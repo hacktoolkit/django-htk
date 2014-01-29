f82ce617fbe2bb501a46771352f9ed78a513f7ff
Jonathan Tsai <jontsai@jonathantsai.com>
2014-01-29 16:09:43 +0000

    fixed HtkSocialAuthExceptionMiddleware

--- apps/accounts/middleware.py
+++ apps/accounts/middleware.py
@@ -1,6 +1,6 @@
-from django.conf import settings
-
 from social.apps.django_app.middleware import SocialAuthExceptionMiddleware
+
+from htk.utils import htk_setting
 
 class HtkSocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
     def get_redirect_uri(self, request, exception):
@@ -10,9 +10,9 @@ class HtkSocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
         However, if user is logged in when the exception occurred, it is a connection failure
         Therefore, always go to account settings page or equivalent
         """
-        url = settings.LOGIN_ERROR_URL
+        default_url = super(HtkSocialAuthExceptionMiddleware, self).get_redirect_uri(request, exception)
         if request.user.is_authenticated():
-            url = htk_setting('HTK_SOCIAL_AUTH_CONNECT_ERROR_URL', settings.LOGIN_ERROR_URL)
+            url = htk_setting('HTK_SOCIAL_AUTH_CONNECT_ERROR_URL', default_url)
         else:
-            url = super(HtkSocialAuthExceptionMiddleware, self).get_redirect_uri(request, exception)
+            url = default_url
         return url
