b1af2c8f76bcb477443a01e87cdd9454b5b26968
Jonathan Tsai <jontsai@pkkup.com>
2014-02-14 16:10:46 +0000

    use os.path.realpath

--- lib/geoip/constants.py
+++ lib/geoip/constants.py
@@ -2,5 +2,5 @@ import os
 
 from django.conf import settings
 
-GEOIP_COUNTRY_DB = os.path.join(settings.BASEDIR, '..', 'conf', 'geoip', 'GeoIP.dat')
-GEOIP_CITY_DB = os.path.join(settings.BASEDIR, '..', 'conf', 'geoip', 'GeoIPCity.dat')
+GEOIP_COUNTRY_DB = os.path.realpath(os.path.join(settings.BASEDIR, '..', 'conf', 'geoip', 'GeoIP.dat'))
+GEOIP_CITY_DB = os.path.realpath(os.path.join(settings.BASEDIR, '..', 'conf', 'geoip', 'GeoIPCity.dat'))
