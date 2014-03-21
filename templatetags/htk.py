31d832cdc39b94a14c5c12c5553388f7ec81df81
jontsai <jontsai@talentral.com>
2014-03-21 22:17:11 +0000

    add lesscss templatetag

--- templatetags/htk.py
+++ templatetags/htk.py
@@ -7,3 +7,13 @@ register = Library()
 def concat(value, arg):
     result = str(value) + str(arg)
     return result
+
+@register.simple_tag(takes_context=True)
+def lesscss(context, css_file_path_base):
+    values = {
+        'css_rel' : context.get('css_rel', 'stylesheet'),
+        'css_ext' : context.get('css_ext', 'css'),
+        'css_file_path_base' : css_file_path_base,
+    }
+    html = '<link type="text/css" rel="%(css_rel)s" href="%(css_file_path_base)s.%(css_ext)s" />' % values
+    return html
