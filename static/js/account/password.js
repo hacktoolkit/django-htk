04434945b717ab4e21e34388ce3b506018392cc6
Jonathan Tsai <jontsai@pkkup.com>
2014-03-30 19:05:17 +0000

    split off emails.js and password.js from settings.js

---
+++ static/js/account/password.js
@@ -0,0 +1,148 @@
+YUI().use(
+    'node',
+    'event',
+    'cookie',
+    'io',
+    'json',
+function (Y) {
+    /* -------------------------------------------------- */
+    /* YUI "Local" Globals */
+
+    // CSS selectors
+    var CSS_CLASS_HIDDEN = 'hidden';
+    var CSS_CLASS_VISIBLE = 'visible';
+    var CSS_CLASS_UPDATE_SUCCESS = 'update-success';
+    var CSS_CLASS_UPDATE_ERROR = 'update-error';
+    var CSS_CLASS_DESELECTED = 'deselected';
+    var CSS_CLASS_MESSAGE = 'message';
+    var CSS_CLASS_SUBMIT_BUTTON = 'submit-button';
+    var CSS_CLASS_CANCEL_BUTTON = 'cancel-button';
+    var CSS_CLASS_DELETE = 'delete';
+    var CSS_CLASS_SET_PRIMARY = 'set-primary';
+
+    // password
+    var CSS_CLASS_PASSWORD_CHANGE = 'password-change';
+    var CSS_CLASS_PASSWORD_FORM_CONTAINER = 'password-form-container';
+    var CSS_CLASS_PASSWORD_MESSAGE_CONTAINER = 'password-message-container';
+
+    var CSS_ID_PAGE_ACCOUNT_SETTINGS = 'page_account_settings';
+    var CSS_ID_PASSWORD_CHANGE_FORM_TEMPLATE = 'password_change_form_template';
+
+    // ----------
+    // Nodes
+    var main = Y.one('#main');
+    var settings = main.one('#' + CSS_ID_PAGE_ACCOUNT_SETTINGS);
+    // password
+    var passwordFormContainer = settings.one('.' + CSS_CLASS_PASSWORD_FORM_CONTAINER);
+    var passwordMessageContainer = settings.one('.' + CSS_CLASS_PASSWORD_MESSAGE_CONTAINER);
+    var passwordFormTemplate = settings.one('#' + CSS_ID_PASSWORD_CHANGE_FORM_TEMPLATE);
+
+    // App variables
+    var SETTINGS_FIELD_CACHE = {};
+    var IO_TRANSACTION_DATA = {};
+    var URI_SET_AVATAR = '/api/account/avatar';
+    var S_CONFIRM_DELETE_EMAIL = 'Are you sure you want to delete this email address, ';
+
+    /* End YUI "Local" Globals */
+    /* -------------------------------------------------- */
+
+    // Custom App Functions
+
+    function handleEnterKeyPressed(e) {
+        e.preventDefault();
+    }
+
+    // --------------------------------------------------
+    // Change Password Functions
+
+    function showPasswordForm() {
+        passwordFormContainer.setHTML(passwordFormTemplate.getHTML());
+    }
+
+    function hidePasswordForm() {
+        passwordFormContainer.setHTML();
+    }
+
+    function clearPasswordMessage() {
+        passwordMessageContainer.setHTML();
+    }
+
+    function submitPasswordForm() {
+        var passwordForm = passwordFormContainer.one('form');
+        var uri = passwordForm.get('action');
+        var csrftoken = Y.Cookie.get('csrftoken');
+        var cfg = {
+            method: 'POST',
+            data: null,
+            headers: {
+                'X-CSRFToken': csrftoken
+            },
+            form: {
+                id: passwordForm,
+                useDisabled: false
+            },
+            on: {
+                complete: handleChangePasswordResponse
+            }
+        }
+        var request = Y.io(uri, cfg);
+        var transactionId = request.id;
+        var transactionData = {
+            uri: uri,
+        };
+        IO_TRANSACTION_DATA[transactionId] = transactionData;
+    }
+
+    function handleChangePasswordResponse(transactionId, response) {
+        var responseData = null;
+        try {
+            responseData = Y.JSON.parse(response.responseText);
+        } catch (e) {
+            _rollbar.push('Bad JSON response received from Update Password API Call');
+        }
+        if (responseData) {
+            var status = responseData['status'];
+            if (status === 'okay') {
+                hidePasswordForm();
+                passwordMessageContainer.addClass(CSS_CLASS_UPDATE_SUCCESS);
+                passwordMessageContainer.removeClass(CSS_CLASS_UPDATE_ERROR);
+                passwordMessageContainer.setHTML('Password changed successfully.');
+            } else if (status === 'error') {
+                passwordMessageContainer.addClass(CSS_CLASS_UPDATE_ERROR);
+                passwordMessageContainer.removeClass(CSS_CLASS_UPDATE_SUCCESS);
+                passwordMessageContainer.setHTML('An error occurred while trying to change the password.');
+            } else {
+                // impossible case, do nothing
+            }
+        }
+    }
+
+    function handleChangePasswordPressed(e) {
+        showPasswordForm();
+        clearPasswordMessage();
+    }
+
+    function handleChangePasswordSubmitPressed(e) {
+        submitPasswordForm();
+    }
+
+    function handleChangePasswordCancelPressed(e) {
+        hidePasswordForm();
+        clearPasswordMessage();
+    }
+
+    // App Initializers
+    function initEventHandlers() {
+        // settings field forms
+        settings.delegate('key', handleEnterKeyPressed, 'down:enter', 'input');
+        // password
+        settings.delegate('tap', handleChangePasswordPressed, 'a.' + CSS_CLASS_PASSWORD_CHANGE);
+        settings.delegate('tap', handleChangePasswordSubmitPressed, '.' + CSS_CLASS_PASSWORD_FORM_CONTAINER + ' a.' + CSS_CLASS_SUBMIT_BUTTON);
+        settings.delegate('tap', handleChangePasswordCancelPressed, '.' + CSS_CLASS_PASSWORD_FORM_CONTAINER + ' a.' + CSS_CLASS_CANCEL_BUTTON);
+    }
+
+    function init() {
+    }
+    initEventHandlers();
+    init();
+});
