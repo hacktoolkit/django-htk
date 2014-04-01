327fad8124ac46c02a2a94cb0fc987ddb09948e1
Jonathan Tsai <jontsai@pkkup.com>
2014-04-01 21:06:07 +0000

    adding error templates, test views, and tests

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls import patterns
 from django.conf.urls import url
@@ -12,3 +13,11 @@ urlpatterns = patterns(
     url(r'^BingSiteAuth\.xml$', views.bing_site_auth, name='bing_site_auth'),
     url(r'^redir$', views.redir, name='redir'),
 )
+
+if settings.ENV_DEV:
+    urlpatterns += patterns(
+        '',
+        url(r'^403$', views.error_view, name='error_403'),
+        url(r'^404$', views.error_view, name='error_404'),
+        url(r'^500$', views.error_view, name='error_500'),
+    )
