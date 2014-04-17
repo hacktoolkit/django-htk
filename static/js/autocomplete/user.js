05693bdafa37b8f85a7faed4742baea5a5bf2a5b
Jonathan Tsai <jontsai@pkkup.com>
2014-04-17 02:12:37 +0000

    added user autocomplete js

---
+++ static/js/autocomplete/user.js
@@ -0,0 +1,81 @@
+YUI().add(
+    'htk-user-autocomplete',
+function(Y) {
+
+    // --------------------------------------------------
+    // Internal functions
+
+    function userAutoCompleteResultListLocator(response) {
+        var results = (response && response.data && response.data.results) || [];
+        return results;
+    };
+
+    function userAutoCompleteFilterHasUsernameAndName(query, results) {
+        // Iterate through the array of results and return a filtered
+        // array containing only results that have a non-empty username and name
+        return Y.Array.filter(results, function(result) {
+            var user = result.raw
+            var hasUsernameAndName = Y.Lang.trim(user.username) !== '' && Y.Lang.trim(user.first_name + ' ' + user.last_name) !== '';
+            return hasUsernameAndName;
+        });
+    };
+
+    var userAutoCompleteResultTemplate = '{display_name} ({username})';
+
+    function userAutoCompleteTextLocator(result) {
+        var user = result;
+        var text = Y.Lang.sub(userAutoCompleteResultTemplate, user);
+        return text;
+    }
+    function userAutoCompleteResultFormatter(query, results) {
+        // Iterate over the array of result objects and return an
+        // array of HTML strings
+        return Y.Array.map(results, function(result) {
+            var user = result.raw;
+            // user is an object with keys:
+            // username
+            // first_name
+            // last_name
+            // display_name
+            // gravatar_hash
+
+            // Use string substitution to fill out the User template and
+            // return an HTML string for this result
+            return Y.Lang.sub(userAutoCompleteResultTemplate, user);
+        });
+    }
+
+    // --------------------------------------------------
+    // Public functions
+
+    function init(node, source) {
+        node.plug(Y.Plugin.AutoComplete, {
+            minQueryLength: 1,
+            queryDelay: 300,
+            resultFilters: [userAutoCompleteFilterHasUsernameAndName],
+//            resultFormatter: userAutoCompleteResultFormatter,
+            //resultHighlighter: 'phraseMatch',
+            resultHighlighter: function(query, results) {
+                return Y.Array.map(results, function(result) {
+                    return Y.Highlight.all(result.text, query);
+                });
+            },
+            resultListLocator: userAutoCompleteResultListLocator,
+            resultTextLocator: userAutoCompleteTextLocator,
+            source: source
+        });
+    }
+
+    Y.UserAutoComplete = {
+        init : init
+    }
+
+}, '0.1.0', {
+    requires: [
+        'node',
+        'event',
+        'autocomplete',
+        'autocomplete-highlighters',
+        'highlight'
+    ]
+});
