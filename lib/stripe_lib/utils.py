9448086d48a99ab3a1ec5e028fe275e00360c499
jontsai <jontsai@talentral.com>
2014-09-19 08:18:03 +0000

    commented out debugging stuff; just directlyr report exception to rollbar

--- lib/stripe_lib/utils.py
+++ lib/stripe_lib/utils.py
@@ -59,15 +59,15 @@ def safe_stripe_call(func, *args, **kwargs):
         result = func(*args, **kwargs)
     except stripe.error.CardError, e:
         # Since it's a decline, stripe.error.CardError will be caught
-        body = e.json_body
-        err  = body['error']
+        #body = e.json_body
+        #err  = body['error']
 
-        print "Status is: %s" % e.http_status
-        print "Type is: %s" % err['type']
-        print "Code is: %s" % err['code']
-        # param is '' in this case
-        print "Param is: %s" % err['param']
-        print "Message is: %s" % err['message']
+        #print "Status is: %s" % e.http_status
+        #print "Type is: %s" % err.get('type')
+        #print "Code is: %s" % err.get('code')
+        ## param is '' in this case
+        #print "Param is: %s" % err.get('param')
+        #print "Message is: %s" % err.get('message')
         rollbar.report_exc_info()
     except stripe.error.InvalidRequestError, e:
         # Invalid parameters were supplied to Stripe's API
