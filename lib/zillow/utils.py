1e74c87b1e145af1910c73d9a0f1c236e8a2c816
Jonathan Tsai <jontsai@jonathantsai.com>
2015-10-14 04:25:38 +0000

    add zillow zestimate api util

---
+++ lib/zillow/utils.py
@@ -0,0 +1,52 @@
+# Zillow API
+# http://www.zillow.com/howto/api/APIOverview.htm
+
+from bs4 import BeautifulSoup
+import requests
+
+from htk.utils import htk_setting
+
+def parse_zestimate_response(response):
+    soup = BeautifulSoup(response.content, 'xml')
+    zresponse = soup.response
+    zestimate = {
+        'zpid' : zresponse.zpid.string,
+        'url' : zresponse.links.homedetails.string,
+        'address' : {
+            'street' : zresponse.address.street.string,
+            'zipcode' : zresponse.address.zipcode.string,
+            'city' : zresponse.address.city.string,
+            'state' : zresponse.address.state.string,
+            'latitude' : zresponse.address.latitude.string,
+            'longitude' : zresponse.address.longitude.string,
+        },
+        'zestimate' : {
+            'amount' : zresponse.zestimate.amount.string,
+            'last_updated' : zresponse.zestimate.find('last-updated').string,
+            'value_change' : zresponse.zestimate.valueChange.string,
+            'low' : zresponse.zestimate.valuationRange.low.string,
+            'high' : zresponse.zestimate.valuationRange.high.string,
+        },
+    }
+    return zestimate
+
+def get_zestimate(zpid, zwsid=None):
+    """Get the Zestimate for `zpid`
+    `zpid` Zillow property id
+
+    http://www.zillow.com/howto/api/GetZestimate.htm
+    """
+    if zwsid is None:
+        zwsid = htk_setting('HTK_ZILLOW_ZWSID')
+
+    url = 'http://www.zillow.com/webservice/GetZestimate.htm'
+    params = {
+        'zws-id' : zwsid,
+        'zpid' : zpid,
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        zestimate = parse_zestimate_response(response)
+    else:
+        zestimate = None
+    return zestimate
