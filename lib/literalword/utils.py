aeaeed8ea83d87bda68ce3df852166a0fe0bfb77
Jonathan Tsai <jontsai@jonathantsai.com>
2015-10-15 04:31:44 +0000

    add helpful error message if passage not found

--- lib/literalword/utils.py
+++ lib/literalword/utils.py
@@ -11,16 +11,22 @@ def get_bible_passage(query):
         'q' : query,
     }
     response = requests.get(url, params)
+    default_text = 'Could not find passage in Bible. Please review your query or try again later.'
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        container = soup.select('.bMeatWrapper')[0]
-        # remove <p> tags
-        for p in container.find_all('p'):
-            p.decompose()
-        html = u'%s' % container
-        text = html2markdown(html)
+        text_container = soup.select('.bMeatWrapper')
+        if len(text_container):
+            container = text_container
+            # remove <p> tags
+            for p in container.find_all('p'):
+                p.decompose()
+            html = u'%s' % container
+            text = html2markdown(html)
+        else:
+            text = default_text
     else:
-        text = 'Could not find passage. Please review your query or try again later.'
+        text = default_text
+
     passage = {
         'url' : response.url,
         'text' : text,
