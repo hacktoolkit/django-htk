81afaea47f6517b35dda1ae74fe726078601febb
Jonathan Tsai <jontsai@jonathantsai.com>
2015-12-30 23:27:05 +0000

    add generate_html_from_template util function

--- utils/templates.py
+++ utils/templates.py
@@ -8,3 +8,17 @@ def get_renderer():
 def get_template_context_generator():
     wrap_data = resolve_method_dynamically(htk_setting('HTK_TEMPLATE_CONTEXT_GENERATOR'))
     return wrap_data
+
+def generate_html_from_template(template_name, context_dict):
+    """Generate HTML by using `template_name` inflated with `context_dict`
+    """
+    from django.template import Context
+    from django.template import TemplateDoesNotExist
+    from django.template import loader
+    try:
+        template = loader.get_template(template_name)
+        context = Context(context_dict)
+        html = template.render(context)
+    except TemplateDoesNotExist:
+        html = ''
+    return html
