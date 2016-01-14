fb7f3f65445abeff70a991ff5dcddd9c49496db2
Jonathan Tsai <jontsai@jonathantsai.com>
2016-01-14 03:20:23 +0000

    add qrcode_image view

---
+++ lib/qrcode/views.py
@@ -0,0 +1,13 @@
+def qrcode_image(request):
+    qr_key = request.GET.get('key', None)
+    qr_data = request.GET.get('data', None)
+
+    if qr_key and qr_data:
+        from htk.lib.qrcode.utils import restricted_qrcode_image_response
+        response = restricted_qrcode_image_response(data=qr_data, key=qr_key)
+    else:
+        response = None
+
+    if response is None:
+        raise Http404
+    return response
