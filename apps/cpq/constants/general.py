647e623d4aa3abca99e69bb57914fd5c8601726e
Jonathan Tsai <jontsai@jonathantsai.com>
2016-01-14 04:47:12 +0000

    update app links

--- apps/cpq/constants/general.py
+++ apps/cpq/constants/general.py
@@ -1,9 +1,10 @@
+# https://docs.djangoproject.com/en/dev/ref/contrib/admin/#reversing-admin-urls
 CPQ_APP_MODEL_NAMES = [
-    'invoice',
-    'quote',
-    'customer',
     'organizationcustomer',
+    'customer',
     'postaladdress',
+    'quote',
+    'invoice',
 ]
 
 CPQ_REPORTING_URL_NAMES = [
