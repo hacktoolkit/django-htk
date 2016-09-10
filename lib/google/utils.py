2239a916afb177e9083f2f2da98e735a9abf6895
Jonathan Tsai <jontsai@jonathantsai.com>
2016-09-10 09:49:59 +0000

    add get_num_server_api_keys method

--- lib/google/utils.py
+++ lib/google/utils.py
@@ -2,6 +2,18 @@ import random
 
 from htk.utils import htk_setting
 
+def get_num_server_api_keys():
+    """Returns the number of Google server API keys configured
+    """
+    key = htk_setting('HTK_GOOGLE_SERVER_API_KEY')
+    if type(key) == str:
+        num = 1
+    elif hasattr(key, '__iter__'):
+        num = len(key)
+    else:
+        num = 0
+    return num
+
 def get_server_api_key(use_pool=False):
     """Retrieves the Google Server API key
 
