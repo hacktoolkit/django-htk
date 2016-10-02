f52d34122f2506f7ecc51e2c1409e557066eabe5
Jonathan Tsai <jontsai@jonathantsai.com>
2016-10-02 15:11:52 +0000

    update esvapi options

--- lib/esv/utils.py
+++ lib/esv/utils.py
@@ -6,9 +6,13 @@ def esvapi_get_passage(passage, key='IP'):
     params = {
         'key' : key,
         'passage' : passage,
+        'include-footnotes' : 0,
+        'include-copyright' : 0,
         'include-short-copyright' : 0,
         'include-passage-horizontal-lines' : 0,
         'include-heading-horizontal-lines' : 0,
+        'audio-format' : 'mp3',
+        'audio-version' : 'mm',
     }
     response = requests.get(ESV_API_PASSAGE_QUERY_URL, params=params)
     html = response.content
