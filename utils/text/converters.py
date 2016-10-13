a692764783a420243b999569590edd3b17b9a4f2
Jonathan Tsai <hello@jontsai.com>
2016-10-13 03:23:38 +0000

    improve markdown2slack -- handle italics

--- utils/text/converters.py
+++ utils/text/converters.py
@@ -14,10 +14,14 @@ def markdown2slack(markdown_text):
     slack_lines = []
     for line in markdown_lines:
         line = line.strip()
+        # MD bold to intermediate bold markup (to prevent conflict with rewrite of MD italic)
+        line = re.sub(r'\*\*(.+?)\*\*', r'<b>\1<b>', line)
+        # MD italic to Slack italic
+        line = re.sub(r'(\*(.+?)\*)', r'_\1_', line)
+        # intermediate bold markup to Slack bold
+        line = re.sub(r'<b>(.+?)<b>', r'*\1*', line)
         # MD headings to Slack bold
         line = re.sub(r'^#+(.*)$', r'*\1*', line)
-        # MD bold to Slack bold
-        line = re.sub(r'\*\*(.*?)\*\*', r'*\1*', line)
         slack_lines.append(line)
     slack_text = '\n'.join(slack_lines)
     return slack_text
