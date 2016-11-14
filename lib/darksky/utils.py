9656e31cc00c1432fb55c00e64f3e2e5481482f3
Jonathan Tsai <hello@jontsai.com>
2016-11-14 07:11:08 +0000

    update weather formatting and rain alert

--- lib/darksky/utils.py
+++ lib/darksky/utils.py
@@ -19,33 +19,47 @@ def get_weather(lat, lng):
         weather = None
     return weather
 
+def _extract_period_weather(period_weather, prefix):
+    """Returns a dictionary of relevant weather data from `period_weather`, with keys prefixed with `prefix`
+    """
+    precip_probability = period_weather['precipProbability']
+    data = {
+        prefix + '_summary' : period_weather['summary'],
+        prefix + '_precip_intensity' : period_weather['precipIntensity'],
+        prefix + '_precip_probability' : '%s%%' % (precip_probability * 100,),
+    }
+    # weather['currently'] only
+    if period_weather.get('temperature'):
+        data[prefix + '_temp'] = period_weather['temperature']
+    # weather['daily']['data'][n] only
+    if period_weather.get('temperatureMax'):
+        data[prefix + '_temp_max'] = period_weather['temperatureMax']
+    if period_weather.get('temperatureMin'):
+        data[prefix + '_temp_min'] = period_weather['temperatureMin']
+
+    # rain alert
+    RAIN_ALERT_THRESHOLD = 0.15
+    if precip_probability > RAIN_ALERT_THRESHOLD:
+        data['rain_alert'] = '\n@here **ALERT!** Likelihood of rain!'
+    return data
+
 def format_weather(weather):
     """Returns Dark Sky (formerly ForecastIO) API `weather` formatted as Markdown
 
     `weather` is the object returned by get_weather()
     """
-    currently = weather['currently']
     daily = weather['daily']['data']
-    today = daily[0]
-    tomorrow = daily[1]
+
     data = {
-        'current_summary' : currently['summary'],
-        'current_temp' : currently['temperature'],
-        'current_precip_intensity' : currently['precipIntensity'],
-        'current_precip_probability' : '%s%%' % (currently['precipProbability'] * 100,),
-        'today_summary' : today['summary'],
-        'today_temp_max' : today['temperatureMax'],
-        'today_temp_min' : today['temperatureMin'],
-        'today_precip_intensity' : today['precipIntensity'],
-        'today_precip_probability' : '%s%%' % (today['precipProbability'] * 100,),
-        'tomorrow_summary' : tomorrow['summary'],
-        'tomorrow_temp_max' : tomorrow['temperatureMax'],
-        'tomorrow_temp_min' : tomorrow['temperatureMin'],
-        'tomorrow_precip_intensity' : tomorrow['precipIntensity'],
-        'tomorrow_precip_probability' : '%s%%' % (tomorrow['precipProbability'] * 100,),
+        'rain_alert' : '',
     }
+    data.update(_extract_period_weather(weather['currently'], 'current'))
+    data.update(_extract_period_weather(daily[0], 'today'))
+    data.update(_extract_period_weather(daily[1], 'tomorrow'))
+
     formatted = u"""**Currently**: %(current_temp)s\xB0F (Precip Intensity: %(current_precip_intensity)s, Probability: %(current_precip_probability)s) - %(current_summary)s  
 **Today**: %(today_temp_max)sF High, %(today_temp_min)s\xB0F Low (Precip Intensity: %(today_precip_intensity)s, Probability: %(today_precip_probability)s) - %(today_summary)s  
 **Tomorrow**: %(tomorrow_temp_max)s\xB0F High, %(tomorrow_temp_min)sF Low (Precip Intensity: %(tomorrow_precip_intensity)s, Probability: %(tomorrow_precip_probability)s) - %(tomorrow_summary)s  
+%(rain_alert)s
 """ % data
     return formatted
