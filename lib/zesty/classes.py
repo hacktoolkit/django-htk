69c650ec216a54b26434be938953758f438c716a
Jonathan Tsai <hello@jontsai.com>
2016-11-24 21:52:48 +0000

    update Zesty API to handle new meals endpoint and meal_items format

--- lib/zesty/classes.py
+++ lib/zesty/classes.py
@@ -1,6 +1,7 @@
 import copy
 import datetime
 import requests
+import rollbar
 
 from htk.lib.zesty.constants import *
 
@@ -29,10 +30,36 @@ class ZestyAPI(object):
                 meals_data = response.json()
                 meals = ZestyMeals(self, meals_data)
             except:
-                pass
+                extra_data = {
+                    'zesty_id' : self.zesty_id,
+                }
+                rollbar.report_exc_info(extra_data=extra_data)
         else:
             pass
         return meals
+
+    def get_meal(self, meal_id):
+        """Retrieves one meal from Zesty API by `meal_id`
+        """
+        meal = None
+        kwargs = {
+            'meal_id' : meal_id,
+        }
+        url = self.get_url('meal', **kwargs)
+        response = requests.get(url)
+        if response.status_code == 200:
+            try :
+                meal_data = response.json()
+                meal = ZestyMeal(self, meal_data)
+            except:
+                extra_data = {
+                    'zesty_id' : self.zesty_id,
+                    'meal_id' : meal_id,
+                }
+                rollbar.report_exc_info(extra_data=extra_data)
+        else:
+            pass
+        return meal
 
     def get_dish(self, dish_id):
         """Retrieves a dish from Zesty API by `dish_id`
@@ -48,12 +75,18 @@ class ZestyAPI(object):
                 dish_data = response.json()
                 dish = ZestyDish(self, dish_data)
             except:
-                pass
+                extra_data = {
+                    'zesty_id' : self.zesty_id,
+                    'dish_id' : dish_id,
+                }
+                rollbar.report_exc_info(extra_data=extra_data)
         else:
             pass
         return dish
 
 class ZestyMeals(object):
+    """Represents a collection of Zesty meals
+    """
     DATE_STRING_FMT = '%Y-%m-%d'
     def __init__(self, api, data):
         """Parse meals data into an internal data structure
@@ -63,14 +96,21 @@ class ZestyMeals(object):
         self.data = data
 
         # store as more useful data structure
-        # meal_items keyed by id
-        self.meal_items = { meal_item['id'] : meal_item for meal_item in data['meal_items'] }
         # meals keyed by YYYY-mm-dd
         self.meals = { meal['delivery_date'][:10] : meal for meal in data['meals'] }
 
     def get_meal_for_date(self, dt):
         date_str = dt.strftime(self.DATE_STRING_FMT)
-        meal = self.meals.get(date_str)
+        meal_data = self.meals.get(date_str)
+        if meal_data:
+            meal = self.api.get_meal(meal_data['id'])
+            if meal is None:
+                # use partial data as a backup if full meal data is not available
+                meal = ZestyMeal(self.api, meal_data)
+            else:
+                pass
+        else:
+            meal = None
         return meal
 
     def get_pretty_menu(self, dt, slack_attachments=False):
@@ -101,7 +141,7 @@ class ZestyMeals(object):
                     target_dt.strftime(self.DATE_STRING_FMT)
                 )
                 menu_str += '\n\n'
-            meal_payload = self.get_pretty_menu_for_meal(meal, slack_attachments=slack_attachments)
+            meal_payload = meal.get_pretty_menu(slack_attachments=slack_attachments)
             menu_str += meal_payload['text']
             attachments = meal_payload['attachments']
         payload = {
@@ -110,26 +150,33 @@ class ZestyMeals(object):
         }
         return payload
 
-    def get_pretty_dishes(self, meal, slack_attachments=False):
-        meal_items = [self.meal_items[meal_item_id] for meal_item_id in meal['meal_item_ids']]
-        pretty_dishes = []
-        for meal_item in meal_items:
-            dish_id = meal_item['dish_id']
-            dish = self.api.get_dish(dish_id)
-            if dish and dish.is_valid:
-                if slack_attachments:
-                    pretty_dish = dish.get_slack_attachment()
-                else:
-                    pretty_dish = dish.get_pretty()
-            else:
-                pretty_dish = '*%s*' % meal_item['name']
-            pretty_dishes.append(pretty_dish)
-        return pretty_dishes
+class ZestyMeal(object):
+    """Represents one Zesty meal for a specific day
+    """
+    def __init__(self, api, data):
+        """Parse meal data into an internal data structure
+        """
+        self.api = api
+        # raw meal metadata as provided by Zesty API JSON response
+        self.data = data
 
-    def _get_meal_dict(self, meal, slack_attachments=False):
+        if 'meals' in data:
+            # from the meal API
+            self.meal = data.get('meals')[0]
+        else:
+            # from the meal metadata from meals API
+            self.meal = data
+
+        # meal_items keyed by id
+        self.meal_items = data.get('meal_items', [])
+
+    def _get_meal_dict(self, slack_attachments=False):
+        """Get menu render data dict
+        """
+        meal = self.meal
         dt = datetime.datetime.strptime(meal['delivery_date'][:16], '%Y-%m-%dT%H:%M')
         meal_dict = copy.copy(meal)
-        dishes = self.get_pretty_dishes(meal, slack_attachments=slack_attachments)
+        dishes = self.get_pretty_dishes(slack_attachments=slack_attachments)
         meal_dict.update({
             'day_of_week' : dt.strftime('%A'),
             'meal_type' : meal['meal_type'].capitalize(),
@@ -139,8 +186,8 @@ class ZestyMeals(object):
         })
         return meal_dict
 
-    def get_pretty_menu_for_meal(self, meal, slack_attachments=False):
-        meal_dict = self._get_meal_dict(meal, slack_attachments=slack_attachments)
+    def get_pretty_menu(self, slack_attachments=False):
+        meal_dict = self._get_meal_dict(slack_attachments=slack_attachments)
         menu_str = """*%(day_of_week)s %(meal_type)s, %(time)s - %(date)s* at %(delivery_location_address)s""" % meal_dict
         attachments = []
         if slack_attachments:
@@ -165,6 +212,23 @@ _%(restaurant_description)s_
             'attachments' : attachments,
         }
         return meal_payload
+
+    def get_pretty_dishes(self, slack_attachments=False):
+        """Makes API calls to fetch individual dish data
+        """
+        pretty_dishes = []
+        for meal_item in self.meal_items:
+            dish_id = meal_item['dish_id']
+            dish = self.api.get_dish(dish_id)
+            if dish and dish.is_valid:
+                if slack_attachments:
+                    pretty_dish = dish.get_slack_attachment()
+                else:
+                    pretty_dish = dish.get_pretty()
+            else:
+                pretty_dish = '*%s*' % meal_item['name']
+            pretty_dishes.append(pretty_dish)
+        return pretty_dishes
 
 class ZestyDish(object):
     def __init__(self, api, data):
--- lib/zesty/constants.py
+++ lib/zesty/constants.py
@@ -2,5 +2,6 @@ ZESTY_URLS = {
     'web_meals' : 'https://meals.zesty.com/clients/%(zesty_id)s/meals',
     'web_meals_today' : 'https://meals.zesty.com/clients/%(zesty_id)s/meals/today',
     'meals' : 'https://api.zesty.com/portal_api/meals?client_id=%(zesty_id)s',
+    'meal' : 'https://api.zesty.com/portal_api/meals/%(meal_id)s',
     'dish' : 'https://api.zesty.com/portal_api/dishes/%(dish_id)s',
 }
