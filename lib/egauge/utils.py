a37acdbd48812af2ff8103eca73fd28bcb64f7c6
Jonathan Tsai <hello@jontsai.com>
2017-02-04 09:13:56 +0000

    better formatting for energy usage data

--- lib/egauge/utils.py
+++ lib/egauge/utils.py
@@ -1,17 +1,26 @@
-def get_daily_energy_summary(egauge_id, unit='kWh'):
+def get_daily_energy_usage_data(egauge_id, unit='kWh', unit_cost=0.13):
     from htk.constants.units import ENERGY_CONVERSION_RATES
     from htk.lib.egauge.classes import EgaugeAPI
+    from htk.utils.currency import moneyfmt
     egauge_api = EgaugeAPI('egauge32798')
     data = egauge_api.get_stored_data()
     (used, generated,) = [-delta for delta in data['values'][1][:2]]
 
-    # convert W*s to desired unit
+    # convert W*s, watt-second, to desired unit
     conversion = '%s_%s' % ('Ws', unit,)
     conversion_rate = ENERGY_CONVERSION_RATES[conversion]
-    summary = {
+    net = generated - used
+    was_profitable = net > 0
+    usage_data = {
         'egauge_id' : egauge_id,
         'used' : used * conversion_rate,
         'generated' : generated * conversion_rate,
-        'net' : (generated - used) * conversion_rate,
+        'unit' : unit,
+        'net' : net * conversion_rate,
+        'was_profitable' : was_profitable,
+        'used_cost' : moneyfmt(used * conversion_rate * unit_cost, curr='$'),
+        'saved_cost' : moneyfmt(generated * conversion_rate * unit_cost, curr='$'),
+        'net_cost' : moneyfmt(net * conversion_rate * unit_cost, curr='$', neg=''),
     }
-    return summary
+    
+    return usage_data
---
+++ utils/currency.py
@@ -0,0 +1,62 @@
+from decimal import Decimal
+
+def moneyfmt(
+    value,
+    places=2,
+    curr='',
+    sep=',',
+    dp='.',
+    pos='',
+    neg='-',
+    trailneg=''
+):
+    """Convert Decimal to a money formatted string.
+
+    places:  required number of places after the decimal point
+    curr:    optional currency symbol before the sign (may be blank)
+    sep:     optional grouping separator (comma, period, space, or blank)
+    dp:      decimal point indicator (comma or period)
+             only specify as blank when places is zero
+    pos:     optional sign for positive numbers: '+', space or blank
+    neg:     optional sign for negative numbers: '-', '(', space or blank
+    trailneg:optional trailing minus indicator:  '-', ')', space or blank
+
+    >>> d = Decimal('-1234567.8901')
+    >>> moneyfmt(d, curr='$')
+    '-$1,234,567.89'
+    >>> moneyfmt(d, places=0, sep='.', dp='', neg='', trailneg='-')
+    '1.234.568-'
+    >>> moneyfmt(d, curr='$', neg='(', trailneg=')')
+    '($1,234,567.89)'
+    >>> moneyfmt(Decimal(123456789), sep=' ')
+    '123 456 789.00'
+    >>> moneyfmt(Decimal('-0.02'), neg='<', trailneg='>')
+    '<0.02>'
+
+    From: https://docs.python.org/3/library/decimal.html#recipes
+    """
+    if type(value) != Decimal:
+        value = Decimal(value)
+    q = Decimal(10) ** -places      # 2 places --> '0.01'
+    sign, digits, exp = value.quantize(q).as_tuple()
+    result = []
+    digits = list(map(str, digits))
+    build, next = result.append, digits.pop
+    if sign:
+        build(trailneg)
+    for i in range(places):
+        build(next() if digits else '0')
+    if places:
+        build(dp)
+    if not digits:
+        build('0')
+    i = 0
+    while digits:
+        build(next())
+        i += 1
+        if i == 3 and digits:
+            i = 0
+            build(sep)
+    build(curr)
+    build(neg if sign else pos)
+    return ''.join(reversed(result))
