981efd08499c1d7e92c2cc5e5abd27cc199d6ca2
Jonathan Tsai <hello@jontsai.com>
2017-02-19 16:40:21 +0000

    add HtkBaseSitemap and refactor HtkStoreSitemap

--- apps/store/sitemaps.py
+++ apps/store/sitemaps.py
@@ -1,36 +1,29 @@
-from django.contrib.sitemaps import Sitemap
-from django.core.urlresolvers import reverse
-
 from itertools import chain
 
+from htk.sitemaps import HtkBaseSitemap
 from htk.utils import htk_setting
 from htk.utils import resolve_model_dynamically
 
 Product = resolve_model_dynamically(htk_setting('HTK_STORE_PRODUCT_MODEL'))
 ProductCollection = resolve_model_dynamically(htk_setting('HTK_STORE_PRODUCT_COLLECTION_MODEL'))
 
-class HtkStoreSitemap(Sitemap):
+class HtkStoreSitemap(HtkBaseSitemap):
     priority = 0.5
     changefreq = 'daily'
 
-    def items(self):
+    def get_static_view_names(self):
         static_view_names = [
             'store',
             'store_collections',
             'store_products',
         ]
+        return static_view_names
+
+    def get_model_instances(self):
         products = Product.objects.all()
         product_collections = ProductCollection.objects.all()
-        _items = list(chain(
-            static_view_names,
+        _instances = chain(
             products,
-            product_collections
-        ))
-        return _items
-
-    def location(self, obj):
-        if type(obj) == str:
-            path = reverse(obj)
-        else:
-            path = obj.get_absolute_url()
-        return path
+            product_collections,
+        )
+        return _instances
---
+++ sitemaps.py
@@ -0,0 +1,34 @@
+from django.contrib.sitemaps import Sitemap
+from django.core.urlresolvers import reverse
+
+from itertools import chain
+
+# https://docs.djangoproject.com/en/1.8/ref/contrib/sitemaps/
+
+class HtkBaseSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'daily'
+
+    def get_static_view_names(self):
+        static_view_names = []
+        return static_view_names
+
+    def get_model_instances(self):
+        model_instances = []
+        return model_instances
+
+    def items(self):
+        static_view_names = self.get_static_view_names()
+        model_instances = self.get_model_instances()
+        _items = list(chain(
+            static_view_names,
+            model_instances,
+        ))
+        return _items
+
+    def location(self, obj):
+        if type(obj) == str:
+            path = reverse(obj)
+        else:
+            path = obj.get_absolute_url()
+        return path
