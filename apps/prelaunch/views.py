7360435840bec8f1a83912a1cdc7ffe856486a9c
Jonathan Tsai <jontsai@pkkup.com>
2017-07-04 19:12:10 +0000

    fix import for get_current_site

--- apps/prelaunch/views.py
+++ apps/prelaunch/views.py
@@ -1,4 +1,4 @@
-from django.contrib.sites.models import get_current_site
+from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import redirect
 from django.template.context_processors import csrf
 
