096869b4500af73bf84829452e7ddf73a2226301
Jonathan Tsai <hello@jontsai.com>
2017-09-15 02:08:51 +0000

    allow specifying delay minutes for BART departure lookups

--- lib/sfbart/utils.py
+++ lib/sfbart/utils.py
@@ -1,6 +1,10 @@
+import datetime
+import pytz
+
 from htk.lib.sfbart.api import BartAPI
 from htk.lib.sfbart.constants import *
 from htk.utils import htk_setting
+from htk.utils import utcnow
 
 def get_station_name(station_abbrev):
     station_name = BART_STATION_ABBREVIATIONS.get(station_abbrev, 'Invalid station abbrev: %s' % station_abbrev)
@@ -15,10 +19,18 @@ def get_bart_stations():
     stations = sorted(stations, key=lambda x: x['name'])
     return stations
 
-def get_bart_schedule_depart(orig_station, dest_station):
+def get_bart_schedule_depart(orig_station, dest_station, delay_mins=None):
     api_key = htk_setting('HTK_BART_API_KEY')
     api = BartAPI(api_key)
-    api_result = api.get_schedule_depart(orig_station, dest_station)
+
+    if delay_mins:
+        now = utcnow().astimezone(pytz.timezone('America/Los_Angeles'))
+        depart_time = now + datetime.timedelta(minutes=delay_mins)
+        depart_time_str = depart_time.strftime('%I:%M%p').lower()
+        api_result = api.get_schedule_depart(orig_station, dest_station, time=depart_time_str, trips_before=0)
+    else:
+        api_result = api.get_schedule_depart(orig_station, dest_station)
+
     data = {
         'origin' : orig_station.upper(),
         'destination' : dest_station.upper(),
@@ -27,4 +39,3 @@ def get_bart_schedule_depart(orig_station, dest_station):
     }
     data.update(api_result)
     return data
-
