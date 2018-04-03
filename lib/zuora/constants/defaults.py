341a896f3989ac3aef5279700b7b0465f43db99a
Jonathan Tsai <hello@jontsai.com>
2018-04-03 17:45:10 +0000

    process Zuora webhooks

--- lib/zuora/constants/defaults.py
+++ lib/zuora/constants/defaults.py
@@ -2,3 +2,18 @@ HTK_ZUORA_USERNAME = None
 HTK_ZUORA_PASSWORD = None
 HTK_ZUORA_COUNTRY = 'US' # 'US' or 'EU'
 HTK_ZUORA_PROD = False
+
+HTK_ZUORA_HANDLE_UNHANDLED_EVENTS = False
+
+HTK_ZUORA_EVENT_TYPES = {
+    'default' : 'Default (unhandled) event',
+    # billing
+    # payments
+    'subscription_created' : 'Subscription created',
+    # finance
+}
+
+HTK_ZUORA_EVENT_HANDLERS = {
+    'default' : 'htk.lib.zuora.event_handlers.default',
+    'subscription_created' : 'htk.lib.zuora.event_handlers.subscription_created',
+}
---
+++ lib/zuora/utils.py
@@ -0,0 +1,24 @@
+# Python Standard Library Imports
+
+# Third Party / PIP Imports
+
+# Django Imports
+
+# HTK Imports
+from htk.utils import htk_setting
+from htk.utils.general import resolve_method_dynamically
+
+def get_event_name(event_type):
+    event_types = htk_setting('HTK_ZUORA_EVENT_TYPES')
+    event_name = event_types.get(event_type, 'Unknown event')
+    return event_name
+
+def get_event_handler(event_type):
+    event_handlers = htk_setting('HTK_ZUORA_EVENT_HANDLERS')
+    event_type = event_type if event_type in event_handlers else 'default' if htk_setting('HTK_ZUORA_HANDLE_UNHANDLED_EVENTS') else None
+    event_handler_module = event_handlers.get(event_type)
+    if event_handler_module:
+        event_handler = resolve_method_dynamically(event_handler_module)
+    else:
+        event_handler = None
+    return event_handler
