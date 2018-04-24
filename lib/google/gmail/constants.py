544071d4f14c0aad91abc6e5a517ccb1a214cf11
Jonathan Tsai <hello@jontsai.com>
2018-04-24 15:44:50 +0000

    add gmail list messages and get message API

--- lib/google/gmail/constants.py
+++ lib/google/gmail/constants.py
@@ -1,3 +1,5 @@
 GMAIL_RESOURCES = {
-    'labels_list' : 'https://www.googleapis.com/gmail/v1/users/%s/labels',
+    'labels_list' : 'https://www.googleapis.com/gmail/v1/users/%(user_id)s/labels',
+    'message_get' : 'https://www.googleapis.com/gmail/v1/users/%(user_id)s/messages/%(message_id)s',
+    'messages_list' : 'https://www.googleapis.com/gmail/v1/users/%(user_id)s/messages',
 }
--- lib/google/gmail/utils.py
+++ lib/google/gmail/utils.py
@@ -4,8 +4,13 @@ from htk.lib.google.gmail.constants import GMAIL_RESOURCES
 
 # https://developers.google.com/gmail/api/v1/reference/
 
-def get_resource_url(resource_name, email):
-    url = GMAIL_RESOURCES[resource_name] % email
+def get_resource_url(resource_name, email=None, **kwargs):
+    user_id = email if email else 'me'
+    values = {
+        'user_id' : user_id,
+    }
+    values.update(kwargs)
+    url = GMAIL_RESOURCES[resource_name] % values
     return url
 
 def get_authorization_header(user, email):
@@ -49,6 +54,47 @@ def labels_list(user, email):
 ##
 # Users.messages
 # https://developers.google.com/gmail/api/v1/reference/#Users.messages
+
+def messages_list(user, email, q=''):
+    """https://developers.google.com/gmail/api/v1/reference/users/messages/list
+    """
+    url = get_resource_url('messages_list', email)
+    headers = {
+        'Authorization' : get_authorization_header(user, email),
+    }
+    params = {}
+    if q:
+        params['q'] = q
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        response_json = response.json()
+        messages = response_json['messages']
+    elif response.status_code == 401:
+        # unauthorized
+        messages = []
+    else:
+        messages = []
+    return messages
+
+def message_get(user, email, message_id):
+    """https://developers.google.com/gmail/api/v1/reference/users/messages/get
+    """
+    kwargs = {'message_id' : message_id,}
+    url = get_resource_url('message_get', email, **kwargs)
+    headers = {
+        'Authorization' : get_authorization_header(user, email),
+    }
+    params = {}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        response_json = response.json()
+        message = response_json
+    elif response.status_code == 401:
+        # unauthorized
+        message = None
+    else:
+        message = None
+    return message
 
 ##
 # Users.messages.attachments
