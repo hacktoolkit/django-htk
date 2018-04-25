6b818d59a54c35e9ba81d302689e5f9444fb937d
Jonathan Tsai <hello@jontsai.com>
2018-04-25 08:24:30 +0000

    refactor Gmail API, add util to extract message HTML

---
+++ lib/google/gmail/api.py
@@ -0,0 +1,153 @@
+# Python Standard Library Imports
+import time
+
+# Third Party / PIP Imports
+import requests
+
+# HTK Imports
+from htk.lib.google.gmail.constants import GMAIL_RESOURCES
+
+
+class GmailAPI(object):
+    """Interface to Gmail API
+
+    https://developers.google.com/gmail/api/v1/reference/
+    """
+    def __init__(self, user, email=None):
+        self.user = user
+        self.email = email
+
+    def get_resource_url(self, resource_name, **kwargs):
+        user_id = self.email if self.email else 'me'
+        values = {
+            'user_id' : user_id,
+        }
+        values.update(kwargs)
+        url = GMAIL_RESOURCES[resource_name] % values
+        return url
+
+    def get_authorization_header(self):
+        g_social_auth = self.user.profile.get_social_user('google-oauth2', self.email)
+        header = None
+        if g_social_auth:
+            # refreshes token if necessary
+            from social_django.utils import load_strategy
+            access_token = g_social_auth.get_access_token(load_strategy())
+            values = {
+                'token_type' : g_social_auth.extra_data['token_type'],
+                'access_token' : access_token,
+            }
+            header = '%(token_type)s %(access_token)s' % values
+        return header
+
+    def request_get(self, resource_name, headers=None, params=None, resource_args=None):
+        if resource_args is None:
+            resource_args = {}
+        url = self.get_resource_url(resource_name, **resource_args)
+        if headers is None:
+            headers = {}
+        if params is None:
+            params = {}
+        headers.update({
+            'Authorization' : self.get_authorization_header(),
+        })
+        response = requests.get(url, headers=headers, params=params)
+        return response
+
+    ##
+    # Users.drafts
+    # https://developers.google.com/gmail/api/v1/reference/#Users.drafts
+
+    ##
+    # Users.history
+    # https://developers.google.com/gmail/api/v1/reference/#Users.history
+
+    ##
+    # User.labels
+    # https://developers.google.com/gmail/api/v1/reference/#Users.labels
+
+    def labels_list(self):
+        """https://developers.google.com/gmail/api/v1/reference/users/labels/list
+        """
+        resource_name = 'labels_list'
+        response = self.request_get(resource_name)
+        if response.status_code == 200:
+            response_json = response.json()
+            labels = response_json['labels']
+            labels = sorted(labels, key=lambda label: label['name'])
+        elif response.status_code == 401:
+            # unauthorized
+            labels = None
+        else:
+            labels = None
+        return labels
+
+    ##
+    # Users.messages
+    # https://developers.google.com/gmail/api/v1/reference/#Users.messages
+
+    def messages_list(self, q=''):
+        """https://developers.google.com/gmail/api/v1/reference/users/messages/list
+        """
+        resource_name = 'messages_list'
+        params = {}
+        if q:
+            params['q'] = q
+        response = self.request_get(resource_name, params=params)
+        if response.status_code == 200:
+            response_json = response.json()
+            messages = response_json.get('messages', [])
+        elif response.status_code in (400, 401,):
+            # bad request, unauthorized
+            messages = []
+        else:
+            messages = []
+        return messages
+
+    def message_get(self, message_id, params=None):
+        """https://developers.google.com/gmail/api/v1/reference/users/messages/get
+        """
+        resource_name = 'message_get'
+        if params is None:
+            params = {}
+        resource_args = {
+            'message_id' : message_id,
+        }
+        response = self.request_get(resource_name, params=params, resource_args=resource_args)
+        if response.status_code == 200:
+            response_json = response.json()
+            message = response_json
+        elif response.status_code in (400, 401,):
+            # bad request, unauthorized
+            message = None
+        else:
+            message = None
+        return message
+
+    ##
+    # Users.messages.attachments
+    # https://developers.google.com/gmail/api/v1/reference/#Users.messages.attachments
+
+    ##
+    # Users.threads
+    # https://developers.google.com/gmail/api/v1/reference/#Users.threads
+
+    ##
+    # Users.settings
+    # https://developers.google.com/gmail/api/v1/reference/#Users.settings
+
+    ##
+    # Users.settings.forwardingAddresses
+    # https://developers.google.com/gmail/api/v1/reference/#Users.settings.forwardingAddresses
+
+    ##
+    # Users
+    # https://developers.google.com/gmail/api/v1/reference/#Users
+
+    ##
+    # Users.settings.filters
+    # https://developers.google.com/gmail/api/v1/reference/#Users.settings.filters
+
+    ##
+    # Users.settings.sendAs
+    # https://developers.google.com/gmail/api/v1/reference/#Users.settings.sendAs
--- lib/google/gmail/utils.py
+++ lib/google/gmail/utils.py
@@ -1,137 +1,25 @@
 # Python Standard Library Imports
-import time
+import base64
 
 # Third Party / PIP Imports
-import requests
 
 # HTK Imports
-from htk.lib.google.gmail.constants import GMAIL_RESOURCES
 
-# https://developers.google.com/gmail/api/v1/reference/
+def get_message_html(message):
+    """Returns the HTML part of a message from the API
 
-def get_resource_url(resource_name, email=None, **kwargs):
-    user_id = email if email else 'me'
-    values = {
-        'user_id' : user_id,
-    }
-    values.update(kwargs)
-    url = GMAIL_RESOURCES[resource_name] % values
-    return url
-
-def get_authorization_header(user, email):
-    g_social_auth = user.profile.get_social_user('google-oauth2', email)
-    header = None
-    if g_social_auth:
-        # refreshes token if necessary
-        from social_django.utils import load_strategy
-        access_token = g_social_auth.get_access_token(load_strategy())
-        values = {
-            'token_type' : g_social_auth.extra_data['token_type'],
-            'access_token' : access_token,
-        }
-        header = '%(token_type)s %(access_token)s' % values
-    return header
-
-##
-# Users.drafts
-# https://developers.google.com/gmail/api/v1/reference/#Users.drafts
-
-##
-# Users.history
-# https://developers.google.com/gmail/api/v1/reference/#Users.history
-
-##
-# User.labels
-# https://developers.google.com/gmail/api/v1/reference/#Users.labels
-
-def labels_list(user, email):
-    """https://developers.google.com/gmail/api/v1/reference/users/labels/list
+    https://developers.google.com/gmail/api/v1/reference/users/messages/get
     """
-    url = get_resource_url('labels_list', email)
-    headers = {
-        'Authorization' : get_authorization_header(user, email),
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        response_json = response.json()
-        labels = response_json['labels']
-        labels = sorted(labels, key=lambda label: label['name'])
-    elif response.status_code == 401:
-        # unauthorized
-        labels = None
+    html_part = None
+    for part in message['payload']['parts']:
+        if part['mimeType'] == 'text/html':
+            html_part = part
+            break
+
+    if html_part:
+        message_body_data = html_part['body']['data']
+        message_html = base64.b64decode(message_body_data.replace('-', '+').replace('_', '/'))
     else:
-        labels = None
-    return labels
+        message_html = None
 
-##
-# Users.messages
-# https://developers.google.com/gmail/api/v1/reference/#Users.messages
-
-def messages_list(user, email, q=''):
-    """https://developers.google.com/gmail/api/v1/reference/users/messages/list
-    """
-    url = get_resource_url('messages_list', email)
-    headers = {
-        'Authorization' : get_authorization_header(user, email),
-    }
-    params = {}
-    if q:
-        params['q'] = q
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        response_json = response.json()
-        messages = response_json.get('messages', [])
-    elif response.status_code in (400, 401,):
-        # bad request, unauthorized
-        messages = []
-    else:
-        messages = []
-    return messages
-
-def message_get(user, email, message_id):
-    """https://developers.google.com/gmail/api/v1/reference/users/messages/get
-    """
-    kwargs = {'message_id' : message_id,}
-    url = get_resource_url('message_get', email, **kwargs)
-    headers = {
-        'Authorization' : get_authorization_header(user, email),
-    }
-    params = {}
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        response_json = response.json()
-        message = response_json
-    elif response.status_code in (400, 401,):
-        # bad request, unauthorized
-        message = None
-    else:
-        message = None
-    return message
-
-##
-# Users.messages.attachments
-# https://developers.google.com/gmail/api/v1/reference/#Users.messages.attachments
-
-##
-# Users.threads
-# https://developers.google.com/gmail/api/v1/reference/#Users.threads
-
-##
-# Users.settings
-# https://developers.google.com/gmail/api/v1/reference/#Users.settings
-
-##
-# Users.settings.forwardingAddresses
-# https://developers.google.com/gmail/api/v1/reference/#Users.settings.forwardingAddresses
-
-##
-# Users
-# https://developers.google.com/gmail/api/v1/reference/#Users
-
-##
-# Users.settings.filters
-# https://developers.google.com/gmail/api/v1/reference/#Users.settings.filters
-
-##
-# Users.settings.sendAs
-# https://developers.google.com/gmail/api/v1/reference/#Users.settings.sendAs
+    return message_html
