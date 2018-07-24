d6bf05665c85eea98e5e17e446f66fe7f167f926
ajnassar <ajnassar@gmail.com>
2018-07-24 12:15:04 -0700

    Removes comment, incorrect and not needed

--- templatetags/htk_tags.py
+++ templatetags/htk_tags.py
@@ -208,8 +208,6 @@ def get_django_setting(context, key):
 
 @register.simple_tag()
 def get_htk_setting(key):
-    """Retrieves a Django setting and sets it on the context dictionary
-    """
     from htk.utils import htk_setting
     value = htk_setting(key)
     return value
