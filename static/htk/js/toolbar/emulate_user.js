be65429daf550639f54ca18eab5afeffa5f80b1a
ajnassar <ajnassar@gmail.com>
2018-07-27 11:29:49 -0700

    Add basic emulate functionality with new input

--- static/htk/js/toolbar/emulate_user.js
+++ static/htk/js/toolbar/emulate_user.js
@@ -9,15 +9,22 @@ $(function() {
     var EMULATE_USERNAME = 'emulate_user_username';
     var HTK_EMULATE_USER_COOKIE_EXPIRE_TIME_SECONDS = HTK_EMULATE_USER_COOKIE_EXPIRATION_MINUTES * 60 * 1000;
 
-    // User ID elements
-    var emulateUserIDButton = $('.emulate-user-id-form .emulate-button');
-    var emulateUserIDContainer = $('.emulate-user-id');
-    var emulateUserIDStopButton = $('.emulate-user-id-stop');
+    // Elements
+    var emulateUserForm = $('.emulate-user-form');
+    var emulateUserIDButton = $('.emulate-user-form button.user-id');
+    var emulateUsernameButton = $('.emulate-user-form button.username');
+    var emulateUserStopButton = $('.emulate-user-stop');
+    var emulateUserInput = $('.emulate-user-form input');
 
-    // Username elements
-    var emulateUsernameContainer = $('.emulate-username');
-    var emulateUsernameButton = $('.emulate-username-form .emulate-button');
-    var emulateUsernameStopButton = $('.emulate-username-stop');
+    function getActiveCookie() {
+        return $.cookie(EMULATE_USER_ID) || $.cookie(EMULATE_USERNAME);
+    }
+
+    function getExpireDate() {
+        var date = new Date();
+        date.setTime(date.getTime() + HTK_EMULATE_USER_COOKIE_EXPIRE_TIME_SECONDS);
+        return date;
+    }
 
     function setPulsatingHtkToolBarHandle() {
         if ($.cookie(EMULATE_USER_ID) || $.cookie(EMULATE_USERNAME)) {
@@ -27,51 +34,30 @@ $(function() {
         }
     }
 
-    function toggleContainers() {
-        if ($.cookie(EMULATE_USER_ID)) {
-            emulateUsernameContainer.hide();
-        } else if ($.cookie(EMULATE_USERNAME)) {
-            emulateUserIDContainer.hide();
-        } else {
-            emulateUserIDContainer.show();
-            emulateUsernameContainer.show();
-        }
-    }
-
-    function toggleForm(formName) {
-        var form = $('.emulate-' + formName + '-form');
-        var stopButton = $('.emulate-' + formName + '-stop');
-        var cookieName = getCookieName(formName);
-        if ($.cookie(cookieName)) {
-            form.hide();
-            stopButton.show();
+    function toggleForm() {
+        var activeCookie = getActiveCookie();
+        if (activeCookie) {
+            emulateUserForm.hide();
+            emulateUserStopButton.show();
         } else  {
-            form.show();
-            stopButton.hide();
+            emulateUserForm.show();
+            emulateUserStopButton.hide();
         }
     }
 
-    function get_expire_date() {
-        var date = new Date();
-        date.setTime(date.getTime() + HTK_EMULATE_USER_COOKIE_EXPIRE_TIME_SECONDS);
-        return date;
-    }
-
-    function getCookieName(name) {
-        var cookieNameParts = name.split('-');
-        var cookieName = 'emulate_user_' + cookieNameParts[cookieNameParts.length - 1];
-        return cookieName;
-    }
-
-    function handleEmulateUserButtonClicked(buttonName) {
-        var input = $('.emulate-' + buttonName + '-form .emulate-input');
-        var cookieName = getCookieName(buttonName);
-        var cookieVal = input.val();
-        $.cookie(cookieName, cookieVal, { expires: get_expire_date() });
+    function handleEmulateUserButtonClicked(cookieName) {
+        var cookieVal = emulateUserInput.val();
+        $.cookie(cookieName, cookieVal, { expires: getExpireDate() });
         location.reload();
     }
 
-    function handleEmulateUserStopButtonClicked(cookieName) {
+    function handleEmulateUserStopButtonClicked() {
+        var cookieName;
+        if ($.cookie(EMULATE_USER_ID)) {
+            cookieName = EMULATE_USER_ID;
+        } else if ($.cookie(EMULATE_USERNAME)) {
+            cookieName = EMULATE_USERNAME;
+        }
         $.removeCookie(cookieName, null);
         location.reload();
     }
@@ -87,18 +73,15 @@ $(function() {
     }
 
     function initEventHandlers() {
-        emulateUserIDButton.click(handleEmulateUserButtonClicked.bind(null, 'user-id'));
-        emulateUserIDStopButton.click(handleEmulateUserStopButtonClicked.bind(null, EMULATE_USER_ID));
-        emulateUsernameButton.click(handleEmulateUserButtonClicked.bind(null, 'username'));
-        emulateUsernameStopButton.click(handleEmulateUserStopButtonClicked.bind(null, EMULATE_USERNAME));
+        emulateUserIDButton.click(handleEmulateUserButtonClicked.bind(null, EMULATE_USER_ID));
+        emulateUsernameButton.click(handleEmulateUserButtonClicked.bind(null, EMULATE_USERNAME));
+        emulateUserStopButton.click(handleEmulateUserStopButtonClicked);
         htkToolbarTab.click(handleToggleToolbar);
         htkToolbarHideButton.click(handleToggleToolbar);
     }
 
     function init() {
-        toggleContainers();
-        toggleForm('user-id');
-        toggleForm('username');
+        toggleForm();
         setPulsatingHtkToolBarHandle();
     }
 
