6846f8b8380f7c638a868ffaae752e01d6c91188
Jonathan Tsai <hello@jontsai.com>
2018-07-28 17:40:43 +0000

    refactor emulate users middleware, clean up logic

--- admintools/middleware.py
+++ admintools/middleware.py
@@ -1,24 +1,60 @@
-from htk.admintools.utils import can_emulate_another_user
-from htk.admintools.utils import can_emulate_user
+# Python Standard Library Imports
+
+# Third Party / PIP Imports
+import rollbar
+
+# HTK Imports
+from htk.admintools.utils import is_allowed_to_emulate_users
+from htk.admintools.utils import is_allowed_to_emulate
 
 class HtkEmulateUserMiddleware(object):
     def process_request(self, request):
-        if can_emulate_another_user(request.user):
+        """Replace the authenticated `request.user` if properly emulating
+        """
+        if is_allowed_to_emulate_users(request.user):
             from htk.apps.accounts.utils import get_user_by_id
             from htk.apps.accounts.utils import get_user_by_username
+
             user_id = request.COOKIES.get('emulate_user_id')
             username = request.COOKIES.get('emulate_user_username')
-            if user_id or username:
+
+            is_attempting_to_emulate = user_id or username
+
+            if is_attempting_to_emulate:
                 if user_id:
-                    emulated_user = get_user_by_id(user_id)
+                    targeted_user = get_user_by_id(user_id)
                 elif username:
-                    emulated_user = get_user_by_username(username)
-                if can_emulate_user(request.user, emulated_user):
-                    request.original_user = request.user
-                    request.user = emulated_user
+                    targeted_user = get_user_by_username(username)
+                else:
+                    rollbar.report_message('Impossible case: attempting to emulate another user but not specified')
+
+                if targeted_user is None:
+                    # TODO: message - 'User does not exist'
+                    pass
+                else:
+                    if is_allowed_to_emulate(request.user, targeted_user):
+                        request.original_user = request.user
+                        request.user = targeted_user
+                        # TODO: add message indicating successfully emulated
+                    else:
+                        # TODO: add message indicating failure to emulate
+                        pass
+            else:
+                # not attempting to emulate
+                pass
+        else:
+            # the original user is not allowed to emulate
+            pass
 
     def process_response(self, request, response):
-        if not (hasattr(request, 'original_user') or can_emulate_another_user(request.user)):
+        """Delete user emulation cookies if they should not be set
+        """
+        original_user = getattr(request, 'original_user', None)
+        user = getattr(request, 'user', None)
+        is_emulating = original_user is not None and user is not None
+
+        if not(is_emulating or is_allowed_to_emulate_users(user)):
             response.delete_cookie('emulate_user_id')
             response.delete_cookie('emulate_user_username')
+
         return response
--- admintools/utils.py
+++ admintools/utils.py
@@ -1,9 +1,14 @@
+# Python Standard Library Imports
+
+# Third Party / PIP Imports
 import rollbar
 
+# HTK Imports
 from htk.admintools.cachekeys import HtkCompanyEmployeesCache
 from htk.admintools.cachekeys import HtkCompanyOfficersCache
 from htk.apps.accounts.utils import get_user_by_email
 from htk.utils import htk_setting
+
 
 def get_company_officers_id_email_map():
     """Gets a mapping of company officers
@@ -21,6 +26,7 @@ def get_company_officers_id_email_map():
         c.cache_store(officers_map)
     return officers_map
 
+
 def get_company_employees_id_email_map():
     """Gets a mapping of company employees
 
@@ -37,18 +43,26 @@ def get_company_employees_id_email_map():
         c.cache_store(employees_map)
     return employees_map
 
-def can_emulate_another_user(user):
-    can_emulate = False
+
+def is_allowed_to_emulate_users(user):
+    """Determines whether `user` is allowed to emulate other users
+    """
+    allowed = False
     if user.is_authenticated():
         try:
             user_profile = user.profile
             if user_profile.is_company_officer:
-                can_emulate = True
+                allowed = True
         except:
             rollbar.report_exc_info(request=request)
-    return can_emulate
+    return allowed
 
-def can_emulate_user(original_user, targeted_user):
-    can_emulate = original_user and can_emulate_another_user(original_user) and \
-    targeted_user and not targeted_user.profile.is_company_officer
-    return can_emulate
+
+def is_allowed_to_emulate(original_user, targeted_user):
+    """Determines whether `original_user` is allowed to emulate `targeted_user`
+    """
+    allowed = original_user and \
+              is_allowed_to_emulate_users(original_user) and \
+              targeted_user and \
+              not targeted_user.profile.is_company_officer
+    return allowed
