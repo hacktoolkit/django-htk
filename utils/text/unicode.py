ca87d96b7a1f42a72b091d1b2a52aab3dcc0fa27
Jonathan Tsai <jontsai@talentral.com>
2018-11-01 19:39:01 +0000

    unicode_to_ascii: replace smart apostrophes with regular apostrophes

--- utils/text/unicode.py
+++ utils/text/unicode.py
@@ -1,8 +1,9 @@
 import unicodedata
 
-def unicode_to_ascii(str):
+def unicode_to_ascii(s):
     """Converts a Unicode string to ASCII equivalent if possible
     http://en.wikipedia.org/wiki/Canonical_decomposition#Normalization
     """
-    ascii = unicodedata.normalize('NFKD', str).encode('ascii', 'ignore')
+    s = s.replace(u'\u2019', "'")
+    ascii = unicodedata.normalize('NFKD', s).encode('ascii', 'ignore')
     return ascii
