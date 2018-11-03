1cb93137f050c7c801dd718d430ef68b294e27bc
Jonathan Tsai <hello@jontsai.com>
2018-11-03 07:35:58 +0000

    simplify Fitbit API

--- lib/fitbit/utils.py
+++ lib/fitbit/utils.py
@@ -1,31 +1,27 @@
-from htk.lib.fitbit.api import FitbitAPI
+def get_fitbit_api(user, social_auth_user=None, client_id=None, client_secret=None):
+    from htk.lib.fitbit.api import FitbitAPI
 
-def get_fitbit_api_django(social_auth_user):
-    from django.conf import settings
-    client_id = settings.SOCIAL_AUTH_FITBIT_KEY
-    client_secret = settings.SOCIAL_AUTH_FITBIT_SECRET
-    api = get_fitbit_api(social_auth_user, client_id, client_secret)
-    return api
+    if client_id is None or client_secret is None:
+        # if either is missing, obtain both from Django settings file
+        from django.conf import settings
+        client_id = settings.SOCIAL_AUTH_FITBIT_KEY
+        client_secret = settings.SOCIAL_AUTH_FITBIT_SECRET
 
-def get_fitbit_api(social_auth_user, client_id, client_secret):
+    if social_auth_user is None:
+        from htk.apps.accounts.utils.social_utils import get_social_auth_for_user
+        social_auth_user = get_social_auth_for_user(user, 'fitbit')
+
     api = FitbitAPI(social_auth_user, client_id, client_secret)
     return api
 
-def get_fitbit_social_auth_user(user):
-    from htk.apps.accounts.utils.social_utils import get_social_auth_for_user
-    social_auth_user = get_social_auth_for_user(user, 'fitbit')
-    return social_auth_user
 
 def get_devices_for_user(user, social_auth_user=None):
-    if social_auth_user is None:
-        social_auth_user = get_fitbit_social_auth_user(user)
-    api = get_fitbit_api_django(social_auth_user)
+    api = get_fitbit_api(user, social_auth_user=social_auth_user)
     devices = api.get_devices()
     return devices
 
+
 def get_activity_for_user(user, social_auth_user=None):
-    if social_auth_user is None:
-        social_auth_user = get_fitbit_social_auth_user(user)
-    api = get_fitbit_api_django(social_auth_user)
+    api = get_fitbit_api(user, social_auth_user=social_auth_user)
     activity = api.get_activity_steps_past_month()
     return activity
