b3585ee9b71f3cc9cf8fd97afaceff2f3e06d5b4
Jonathan Tsai <hello@jontsai.com>
2019-01-27 05:45:40 +0000

    migrate error_redirect.js from YUI to jQuery

--- static/htk/js/error_redirect.js
+++ static/htk/js/error_redirect.js
@@ -1,31 +1,26 @@
-YUI().use(
-    'node',
-    'gallery-timer',
-function (Y) {
+$(function() {
     /* -------------------------------------------------- */
-    /* YUI "Local" Globals */
+    /* "Local" Globals */
 
     // CSS selectors
 
     // Nodes
-    var main = Y.one('#main');
+    var main = $('#main');
 
     // App variables
 
-    /* End YUI "Local" Globals */
+    /* End "Local" Globals */
     /* -------------------------------------------------- */
 
     // Custom App Functions
     function redirectHome() {
-        var cfg = {
-            callback: function() {},
-            repeatCount: 5,
-            length: 1000
-        }
         // TODO: animate countdown
-        var t = new Y.Timer(cfg);
-        t.on('timer:stop', function(e) { document.location='/'; } );
-        t.start();
+        setTimeout(
+            function() {
+                document.location = '/';
+            },
+            5000
+        );
     }
 
     // App Initializers
