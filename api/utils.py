234e3a0b30dcf0d81c0bb60d4fe6a98931dce6f4
Jonathan Tsai <hello@jontsai.com>
2019-01-27 09:55:41 +0000

    modify extract_post_params to do QueryDict getlist on both [] and regular formats

--- api/utils.py
+++ api/utils.py
@@ -1,15 +1,21 @@
+# Python Standard Library Imports
 import datetime
 import json
-import rollbar
 from time import mktime
 
+# Third Party / PIP Imports
+import rollbar
+
+# Django Imports
 from django.core import serializers
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse
 from django.http import QueryDict
 
+# HTK Imports
 from htk.api.constants import *
 from htk.models import HtkBaseModel
+
 
 class HtkJSONEncoder(serializers.json.DjangoJSONEncoder):
     def default(self, obj):
@@ -31,6 +37,7 @@ class HtkJSONEncoder(serializers.json.DjangoJSONEncoder):
                 value = -3.14159 # return an absurd value so that we know the object wasn't serializable
         return value
 
+
 def to_json(obj, encoder=HtkJSONEncoder):
     if hasattr(obj, '_meta'):
         if hasattr(obj, '__contains__'):
@@ -40,12 +47,14 @@ def to_json(obj, encoder=HtkJSONEncoder):
     else:
         return json.dumps(obj, cls=encoder)
 
+
 def json_okay():
     data = {
         HTK_API_JSON_KEY_SUCCESS : True,
         HTK_API_JSON_KEY_STATUS : HTK_API_JSON_VALUE_OKAY,
     }
     return data
+
 
 def json_error(data=None):
     if data is None:
@@ -56,11 +65,14 @@ def json_error(data=None):
     })
     return data
 
+
 def json_okay_str():
     return to_json(json_okay())
 
+
 def json_error_str():
     return to_json(json_error())
+
 
 def json_response(obj, encoder=HtkJSONEncoder, status=200):
     # TODO: consider the new django.http.response.JsonResponse in Django 1.7
@@ -68,14 +80,17 @@ def json_response(obj, encoder=HtkJSONEncoder, status=200):
     response = HttpResponse(to_json(obj, encoder=encoder), content_type='application/json', status=status)
     return response
 
+
 def json_response_okay():
     response = json_response(json_okay())
     return response
+
 
 def json_response_error(data=None, status=400):
     data = json_error(data=data)
     response = json_response(data, status=status)
     return response
+
 
 def extract_post_params(post_data, expected_params, list_params=None, strict=True):
     """Extract `expected_params` from `post_data`
@@ -89,7 +104,7 @@ def extract_post_params(post_data, expected_params, list_params=None, strict=Tru
         if strict and param not in post_data:
             raise Exception('Missing param: %s' % param)
         if param in list_params:
-            value = post_data.getlist('%s[]' % param, False)
+            value = post_data.getlist('%s[]' % param, post_data.getlist(param, False))
             if value is not False:
                 value = json.dumps(value)
                 data[param] = value
