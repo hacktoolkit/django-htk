40fbddf6f5aa8eb36d82939e2a45de8cc314b6df
Jonathan Tsai <hello@jontsai.com>
2019-01-27 10:50:53 +0000

    automatically switch to use secure protocol for Gravatar

--- templatetags/gravatar.py
+++ templatetags/gravatar.py
@@ -9,7 +9,8 @@ from django.utils.safestring import mark_safe
 from htk.lib.gravatar.utils import get_gravatar_hash
 
 # http://en.gravatar.com/site/implement/images/
-GRAVATAR_URL_PREFIX = getattr(settings, 'GRAVATAR_URL_PREFIX', 'http://www.gravatar.com')
+GRAVATAR_URL_PROTOCOL = 'https://' if settings.SECURE_SSL_HOST or settings.SECURE_SSL_REDIRECT else 'http://'
+GRAVATAR_URL_PREFIX = getattr(settings, 'GRAVATAR_URL_PREFIX', '%s%s' % (GRAVATAR_URL_PROTOCOL, '//www.gravatar.com',))
 GRAVATAR_DEFAULT_IMAGE = getattr(settings, 'GRAVATAR_DEFAULT_IMAGE', 'mm')
 GRAVATAR_DEFAULT_SIZE = 80
 
