08c51642be45c4161b5d8d0aa86b4bab11d8cfb9
Jonathan Tsai <hello@jontsai.com>
2019-01-28 09:01:21 +0000

    add util find_all_json_paths

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -20,3 +20,27 @@ def find_json_value(obj, path):
             node = node.get(level_key, None)
 
     return value
+
+
+def find_all_json_paths(json_obj):
+    """Returns all valid JSON paths given a `json_obj`
+    """
+    paths = []
+
+    def _walk(node, path=''):
+        if type(node) == dict:
+            for k, v in node.iteritems():
+                next_path = '%s.%s' % (path, k,)
+                _walk(v, path=next_path)
+        elif type(node) == list:
+            for v in node:
+                _walk(v, path=path)
+        elif type(node) in (str, unicode, int,):
+            paths.append(path)
+
+    for k, v in json_obj.iteritems():
+        _walk(v, k)
+
+    paths = sorted(paths)
+
+    return paths
