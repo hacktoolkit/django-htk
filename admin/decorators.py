626bc2febcdbf76f6f2ff23603eeda2728908384
Jonathan Tsai <hello@jontsai.com>
2019-02-06 01:51:44 +0000

    add UsernameAdminProfileLinks decorator for Django admin

--- admin/decorators.py
+++ admin/decorators.py
@@ -4,7 +4,10 @@ from functools import wraps
 # Third Party / PIP Imports
 
 # Django Imports
+from django.core.urlresolvers import reverse
 from django.utils.safestring import mark_safe
+
+# HTK Imports
 
 
 def django_admin_bool_field(func):
@@ -20,3 +23,32 @@ def django_admin_bool_field(func):
         value = yes_icon if bool_value else no_icon
         return mark_safe(value)
     return wrapped
+
+
+class UsernameAdminProfileLinks(object):
+    def __init__(self, user_resolver=None, short_description=None, admin_order_field=None):
+        self.user_resolver = user_resolver
+        self.short_description = short_description
+        self.admin_order_field = admin_order_field
+
+    def __call__(self, fn):
+        @wraps(fn)
+        def wrapped(instance, obj, *args, **kwargs):
+            user = fn(instance, obj)
+
+            if user:
+                value = '<a href="%(admin_url)s">%(username)s</a> (<a href="%(profile_url)s" target="_blank">Profile</a>)' % {
+                    'admin_url' : reverse('admin:auth_user_change', args=(user.id,)),
+                    'username' : user.username,
+                    'profile_url' : user.profile.get_absolute_url(),
+                }
+            else:
+                value = '-'
+            return mark_safe(value)
+
+        if self.short_description:
+            wrapped.short_description = self.short_description
+        if self.admin_order_field:
+            wrapped.admin_order_field = self.admin_order_field
+
+        return wrapped
