06adf70972b72785b66f6a62f67f25dff06e79ee
Jonathan Tsai <jontsai@talentral.com>
2019-10-14 10:15:04 +0000

    fix image url for test

--- lib/qrcode/tests.py
+++ lib/qrcode/tests.py
@@ -5,6 +5,6 @@ class QrcodeLibTestCase(TestCase):
         import urllib
         from PIL import Image
         import StringIO
-        file = StringIO.StringIO(urllib.urlopen('https://fbstatic-a.akamaihd.net/rsrc.php/v2/yi/r/OBaVg52wtTZ.png').read())
+        file = StringIO.StringIO(urllib.urlopen('https://avatars2.githubusercontent.com/u/5404851?s=600&v=4').read())
         img = Image.open(file)
         img = img.crop((0, 0, 400, 600))
