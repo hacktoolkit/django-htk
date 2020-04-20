3c3f5481e90786ec2028a7a2da93b79a9ebda8dc
Jonathan Tsai <jontsai@talentral.com>
2020-04-20 16:22:57 +0000

    add utils.crypto module with AESCipher

---
+++ utils/crypto.py
@@ -0,0 +1,36 @@
+# Python Standard Library Imports
+import base64
+import hashlib
+
+# Third Party (PyPI) Imports
+from Crypto import Random
+from Crypto.Cipher import AES
+
+
+class AESCipher(object):
+    """Class for encrypting and decrypting data using AES256
+
+    Source: https://stackoverflow.com/a/21928790/865091
+    """
+    def __init__(self, key):
+        self.bs = AES.block_size
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
