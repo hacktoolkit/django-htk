570c80ab9821c0c5be9fd9dd6e37bade92495a79
Jonathan Tsai <jontsai@users.noreply.github.com>
2021-04-20 05:25:52 +0000

    add US timezone choices

---
+++ constants/i18n/timezones.py
@@ -0,0 +1,14 @@
+US_TIMEZONE_CHOICES = (
+    ('America/Anchorage', 'US/Alaska', ),
+    ('America/Adak', 'US/Aleutian', ),
+    ('America/Phoenix', 'US/Arizona', ),
+    ('America/Chicago', 'US/Central', ),
+    ('America/Indiana/Indianapolis', 'US/East-Indiana', ),
+    ('America/New_York', 'US/Eastern', ),
+    ('Pacific/Honolulu', 'US/Hawaii', ),
+    ('America/Indiana/Knox', 'US/Indiana-Starke', ),
+    ('America/Detroit', 'US/Michigan', ),
+    ('America/Denver', 'US/Mountain', ),
+    ('America/Los_Angeles', 'US/Pacific', ),
+    ('Pacific/Pago_Pago', 'US/Samoa', ),
+)
