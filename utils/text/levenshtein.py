fe650657bcc1e4eeeb1be95e35778dfea297e092
Sakthivel S <sakthivel.s@aximsoft.com>
2021-04-20 16:30:38 +0530

    Addressed review comments

--- utils/text/levenshtein.py
+++ utils/text/levenshtein.py
@@ -2,35 +2,11 @@
 import numpy
 
 
-EXTRACTED_JOB_TITLES_FILE_PATH = 'zippy/ml/data/job_titles.json'
-MAX_MATCHED_TITLES = 5
-
-
-def normalize_job_title(job_title):
-    normalized_job_title = get_job_title_matches(job_title)
-    return normalized_job_title
-
-
-def get_job_title_matches(job_title):
-    job_titles_list = []
-    matched_job_titles_list = []
-
-    with open(EXTRACTED_JOB_TITLES_FILE_PATH) as f:
-        job_titles = f.readlines()
-
-    for title in job_titles:
-        job_title_distance = getLevenshteinDistance(job_title, title.strip())
-        if job_title_distance >= 10:
-            job_title_distance = 9
-        job_titles_list.append(str(int(job_title_distance)) + "-" + title.strip())
-    job_titles_list.sort()
-
-    for i in range(MAX_MATCHED_TITLES):
-        matched_job_titles_list.append(job_titles_list[i].split("-")[1])
-    return matched_job_titles_list[0]
-
-
 def getLevenshteinDistance(job_title, title_from_json):
+    """The Levenshtein distance algorithm that compares two words.
+
+    Returns a numeric value representing the distance between two words.
+    """
     a = 0
     b = 0
     c = 0
