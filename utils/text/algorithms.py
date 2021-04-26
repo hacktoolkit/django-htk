8b21be2c180682ad27ccca14b97c302c311c025c
Sakthivel S <sakthivel.s@aximsoft.com>
2021-04-26 10:39:34 +0530

    Addressed review comments

--- utils/text/algorithms.py
+++ utils/text/algorithms.py
@@ -5,6 +5,8 @@ import numpy
 def levenshtein_distance(w1, w2):
     """The Levenshtein distance algorithm that compares two words
 
+    https://en.wikipedia.org/wiki/Levenshtein_distance
+
     https://blog.paperspace.com/implementing-levenshtein-distance-word-autocomplete-autocorrect/
 
     Returns an `int` representing the edit distance between two words
@@ -25,12 +27,15 @@ def levenshtein_distance(w1, w2):
             if (w1[x - 1] == w2[y - 1]):
                 edit_distance[x][y] = edit_distance[x - 1][y - 1]
             else:
-                insertion_cost = edit_distance[x][y - 1]
-                deletion_cost = edit_distance[x - 1][y]
-                substitution_cost = edit_distance[x - 1][y - 1]
+                insertion_cost = edit_distance[x][y - 1] + 1
+                deletion_cost = edit_distance[x - 1][y] + 1
+                substitution_cost = edit_distance[x - 1][y - 1] + 1
 
-                min_cost = min(deletion_cost, insertion_cost, substitution_cost)
-                edit_distance[x][y] = min_cost + 1
+                edit_distance[x][y] = min(
+                    deletion_cost,
+                    insertion_cost,
+                    substitution_cost
+                )
 
     result = edit_distance[len(w1)][len(w2)]
     return result
