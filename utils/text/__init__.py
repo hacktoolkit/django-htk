febfdde0b5c778cdee791784465cab1fbfbe3e37
Sakthivel Subramaniam <55696493+sakthivel-axim@users.noreply.github.com>
2021-04-26 10:42:03 +0530

    Add Levenshtein distance algorithm (#308)
    * Add Levenshtein distance algorithm

--- utils/text/__init__.py
+++ utils/text/__init__.py
@@ -1,2 +1,3 @@
 # HTK Imports
+from htk.utils.text.algorithms import levenshtein_distance
 from htk.utils.text.general import *
---
+++ utils/text/algorithms.py
@@ -0,0 +1,41 @@
+# Third Party (PyPI) Imports
+import numpy
+
+
+def levenshtein_distance(w1, w2):
+    """The Levenshtein distance algorithm that compares two words
+
+    https://en.wikipedia.org/wiki/Levenshtein_distance
+
+    https://blog.paperspace.com/implementing-levenshtein-distance-word-autocomplete-autocorrect/
+
+    Returns an `int` representing the edit distance between two words
+    """
+    insertion_cost = 0
+    deletion_cost = 0
+    substitution_cost = 0
+    edit_distance = numpy.zeros((len(w1) + 1, len(w2) + 1))
+
+    for x in range(len(w1) + 1):
+        edit_distance[x][0] = x
+
+    for y in range(len(w2) + 1):
+        edit_distance[0][y] = y
+
+    for x in range(1, len(w1) + 1):
+        for y in range(1, len(w2) + 1):
+            if (w1[x - 1] == w2[y - 1]):
+                edit_distance[x][y] = edit_distance[x - 1][y - 1]
+            else:
+                insertion_cost = edit_distance[x][y - 1] + 1
+                deletion_cost = edit_distance[x - 1][y] + 1
+                substitution_cost = edit_distance[x - 1][y - 1] + 1
+
+                edit_distance[x][y] = min(
+                    deletion_cost,
+                    insertion_cost,
+                    substitution_cost
+                )
+
+    result = edit_distance[len(w1)][len(w2)]
+    return result
