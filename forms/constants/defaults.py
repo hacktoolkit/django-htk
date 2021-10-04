2bbc37e7a8156bf2e34d2bb626bc38a3700ff080
Jonathan Tsai <hello@jontsai.com>
2021-10-04 07:22:46 +0000

    handle additional form input widget types

--- forms/constants/defaults.py
+++ forms/constants/defaults.py
@@ -8,23 +8,37 @@ HTK_FORM_WIDGET_CLASSES = {
         # Checkbox
         'CheckboxInput': 'form-check-input',
         # Text Inputs
+        'ClearableFileInput': 'form-control',
+        'DateInput': 'form-control',
+        'DateTimeInput': 'form-control',
         'EmailInput': 'form-control',
         'NumberInput': 'form-control',
         'PasswordInput': 'form-control',
         'TextInput': 'form-control',
         'Textarea': 'form-control',
         'URLInput': 'form-control',
+        # Select
+        'NullBooleanSelect': 'form-control',
+        'Select': 'form-control',
+        'SelectMultiple': 'form-control',
     },
     'pure': {
         'default': 'pure-input-1',
         # Checkbox
         'CheckboxInput': 'pure-input-1',
         # Text Inputs
+        'ClearableFileInput': 'pure-input-1',
+        'DateInput': 'pure-input-1',
+        'DateTimeInput': 'pure-input-1',
         'EmailInput': 'pure-input-1',
         'NumberInput': 'pure-input-1',
         'PasswordInput': 'pure-input-1',
         'TextInput': 'pure-input-1',
         'Textarea': 'pure-input-1',
         'URLInput': 'pure-input-1',
+        # Select
+        'NullBooleanSelect': 'pure-input-1',
+        'Select': 'pure-input-1',
+        'SelectMultiple': 'pure-input-1',
     },
 }
