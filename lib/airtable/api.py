e1cb021d944781303328000ce47590edb4c847aa
Jonathan Tsai <hello@jontsai.com>
2022-08-14 20:41:10 +0000

    handles pagination when retrieving Airtable records

--- lib/airtable/api.py
+++ lib/airtable/api.py
@@ -1,3 +1,6 @@
+# Python Standard Library Imports
+import time
+
 # Third Party (PyPI) Imports
 import requests
 import rollbar
@@ -35,23 +38,43 @@ class AirtableAPI:
             table=table
         )
 
-        # TODO: handle pagination, 429
-        response = requests.get(url, headers=headers, params=params)
+        # TODO: handle 429
+        should_fetch = True
+        page_num = 0
 
-        response_json = response.json()
-        if 'records' in response_json:
-            records = response_json['records']
-        else:
-            records = []
+        records = []
 
-            extra_data = {
-                'base_id': self.base_id,
-                'table': table,
-                'view_name': view_name,
-                'url': url,
-                'params': params,
-                'response': response_json,
-            }
-            rollbar.report_message('Airtable API error: No records matching query', extra_data=extra_data)
+        # fetch records in a loop for paginated results
+        while should_fetch:
+            if page_num > 0:
+                time.sleep(0.5)
+
+            response = requests.get(url, headers=headers, params=params)
+            response_json = response.json()
+
+            if 'records' in response_json:
+                records.extend(response_json['records'])
+
+                offset = response_json.get('offset')
+                if offset:
+                    should_fetch = True
+                    params['offset'] = offset
+                    page_num += 1
+                else:
+                    if 'offset' in params:
+                        del params['offset']
+                    should_fetch = False
+            else:
+                should_fetch = False
+
+                extra_data = {
+                    'base_id': self.base_id,
+                    'table': table,
+                    'view_name': view_name,
+                    'url': url,
+                    'params': params,
+                    'response': response_json,
+                }
+                rollbar.report_message('Airtable API error: No records matching query', extra_data=extra_data)
 
         return records
