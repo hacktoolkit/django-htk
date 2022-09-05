5367283c605e97c33e0ef8ea7c1854b1e02f1623
Jonathan Tsai <jontsai@users.noreply.github.com>
2022-09-05 23:40:03 +0000

    improves organization decorators to handle conditional responses based on application/json and text/html content types

--- apps/organizations/constants/defaults.py
+++ apps/organizations/constants/defaults.py
@@ -7,11 +7,18 @@ HTK_ORGANIZATION_TEAM_MEMBER_MODEL = 'organizations.OrganizationTeamMember'
 HTK_ORGANIZATION_TEAM_POSITION_MODEL = 'organizations.OrganizationTeamPosition'
 HTK_ORGANIZATION_TEAM_MEMBER_POSITION_MODEL = 'organizations.OrganizationTeamMemberPosition'
 
+
+HTK_ORGANIZATION_READBLE_NAME = 'Organization'
+HTK_ORGANIZATION_SYMBOL = 'org'
+HTK_ORGANIZATION_URL_PK_KEY = 'org_id'
+
+
 HTK_ORGANIZATION_MEMBERS_SORT_ORDER = (
     'user__first_name',
     'user__last_name',
     'user__username',
 )
+
 
 HTK_ORGANIZATION_TEAM_MEMBERS_SORT_ORDER = (
     'user__first_name',
