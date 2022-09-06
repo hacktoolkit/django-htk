01d11d33f22be739a45ae23dd4817b99bee53224
Jonathan Tsai <jontsai@users.noreply.github.com>
2022-09-06 05:26:40 +0000

    Adds Blob Storage app (`blob_storage`) - Adds `AbstractBlobStorage` model - Adds `CompressedBinaryField` model - gzip compresses values upon insertion into DB - gzip decompresses values upon retrieval from DB

---
+++ apps/blob_storage/models.py
@@ -0,0 +1,56 @@
+# Django Imports
+from django.db import models
+from django.http import (
+    FileResponse,
+    HttpResponse,
+)
+
+# HTK Imports
+from htk.fields import CompressedBinaryField
+from htk.utils import htk_setting
+
+
+class AbstractBlobStorage(models.Model):
+    content_type = models.CharField(max_length=64, blank=True)
+    size_bytes = models.PositiveIntegerField(default=0)
+    contents = CompressedBinaryField(
+        max_length=htk_setting('HTK_BLOB_CONTENT_MAX_LENGTH')
+    )
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def create(cls, content_type='', contents=b''):
+        size_bytes = len(contents)
+        blob = cls.objects.create(
+            content_type=content_type,
+            size_bytes=size_bytes,
+            contents=contents
+        )
+        return blob
+
+    def __str__(self):
+        value = '{} - {}'.format(
+            self.__class__.__name__,
+            self.id
+        )
+        return value
+
+    def update_contents(self, contents=b''):
+        size_bytes = len(contents)
+        self.size_bytes = size_bytes
+        self.contents = contents
+        self.save()
+
+    def as_response(self):
+        response = HttpResponse(
+            self.contents,
+            content_type=self.content_type
+        )
+        # response = FileResponse(
+        #     self.contents,
+        #     as_attachment=True,
+        #     filename='blob.jpeg'
+        # )
+        return response
