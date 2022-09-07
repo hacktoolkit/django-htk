d9ee3d8d304d8f21d9258f67551cef1a29007808
Aarthi Selvakumari ShanmugaSundaram <80397858+aarthi-axim@users.noreply.github.com>
2022-09-07 23:21:12 +0530

    Adds support for Python 2 unicode type for Subscription data (#342)
    * Adds support for Python 2 `unicode` type for Subscription data as well as `str`
    
    Co-authored-by: Jonathan Tsai <jontsai@users.noreply.github.com>

--- lib/stripe_lib/models.py
+++ lib/stripe_lib/models.py
@@ -358,9 +358,12 @@ class BaseStripeCustomer(BaseStripeModel):
             item = {
                 'id': stripe_subscription['items']['data'][0]['id'],
             }
-            if type(new_price_or_plan) == str:
+            if (
+                isinstance(new_price_or_plan, str)
+                or isinstance(new_price_or_plan, type(u''))
+            ):
                 item['price'] = new_price_or_plan
-            elif type(new_price_or_plan) == dict:
+            elif isinstance(new_price_or_plan, dict):
                 item['price_data'] = new_price_or_plan
             else:
                 raise Exception('Unsupported price_or_plan type: %s' % type(new_price_or_plan))
@@ -478,9 +481,12 @@ class BaseStripeSubscription(BaseStripeModel):
         _initialize_stripe(live_mode=self.live_mode)
 
         item = {}
-        if type(price_or_plan) == str:
+        if (
+            isinstance(price_or_plan, str)
+            or isinstance(price_or_plan, type(u''))
+        ):
             item['price'] = price_or_plan
-        elif type(price_or_plan) == dict:
+        elif isinstance(price_or_plan, dict):
             item['price_data'] = price_or_plan
         else:
             raise Exception('Unsupported price_or_plan type: %s' % type(price_or_plan))
