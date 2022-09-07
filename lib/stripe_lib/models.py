dfaea205b8dcb16c2be1edba041a884e56244030
Jonathan Tsai <jontsai@users.noreply.github.com>
2022-09-07 17:49:52 +0000

    combine cases

--- lib/stripe_lib/models.py
+++ lib/stripe_lib/models.py
@@ -358,9 +358,10 @@ class BaseStripeCustomer(BaseStripeModel):
             item = {
                 'id': stripe_subscription['items']['data'][0]['id'],
             }
-            if isinstance(new_price_or_plan, str):
-                item['price'] = new_price_or_plan
-            elif isinstance(new_price_or_plan, type(u'')):
+            if (
+                isinstance(new_price_or_plan, str)
+                or isinstance(new_price_or_plan, type(u''))
+            ):
                 item['price'] = new_price_or_plan
             elif isinstance(new_price_or_plan, dict):
                 item['price_data'] = new_price_or_plan
@@ -480,9 +481,10 @@ class BaseStripeSubscription(BaseStripeModel):
         _initialize_stripe(live_mode=self.live_mode)
 
         item = {}
-        if isinstance(price_or_plan, str):
-            item['price'] = price_or_plan
-        elif isinstance(price_or_plan, type(u'')):
+        if (
+            isinstance(price_or_plan, str)
+            or isinstance(price_or_plan, type(u''))
+        ):
             item['price'] = price_or_plan
         elif isinstance(price_or_plan, dict):
             item['price_data'] = price_or_plan
