f8b1e6a0961076925d5c87cca6b6fe95074e2d70
Jonathan Tsai <jontsai@users.noreply.github.com>
2022-09-11 01:02:57 -0700

    Adds Blob Storage app (`blob_storage`) (#343)
    - Adds `AbstractBlobStorage` model
    - Adds `CompressedBinaryField` model
    - gzip compresses values upon insertion into DB
    - gzip decompresses values upon retrieval from DB

--- fields.py
+++ fields.py
@@ -1,8 +1,12 @@
 # Python Standard Library Imports
+import gzip
 from decimal import Decimal
 
 # Django Imports
 from django.db import models
+
+
+# isort: off
 
 
 try:
@@ -11,6 +15,43 @@ except ImportError:
     SOUTH = False
 else:
     SOUTH = True
+
+
+class CompressedBinaryField(models.BinaryField):
+    """A binary field with gzip compression
+
+    See:
+    - https://docs.djangoproject.com/en/4.1/ref/models/fields/#binaryfield
+    - https://gist.github.com/jontsai/57316aecf5397b35d58be55b7e6ea5b0
+
+    NOTE: From above on Abusing BinaryField:
+
+    Although you might think about storing files in the database,
+    consider that it is bad design in 99% of the cases.
+    This field is not a replacement for proper static files handling.
+    """
+
+    description = "A binary field with gzip compression"
+
+    def from_db_value(self, value, expression, connection):
+        """Converts `value` retrieved from DB
+
+        See:
+        - https://docs.djangoproject.com/en/3.2/ref/models/fields/#django.db.models.Field.from_db_value
+        """
+        python_value = None if value is None else gzip.decompress(value)
+        return python_value
+
+    def get_prep_value(self, value):
+        """Converts `value` into format for storage in DB
+
+        See:
+        - https://docs.djangoproject.com/en/3.2/howto/custom-model-fields/#converting-python-objects-to-query-values
+        - https://docs.djangoproject.com/en/3.2/ref/models/fields/#django.db.models.Field.get_prep_value
+        """
+        prep_value = None if value is None else gzip.compress(value)
+        return prep_value
+
 
 class CurrencyField(models.DecimalField):
     def __init__(self, verbose_name=None, name=None, **kwargs):
