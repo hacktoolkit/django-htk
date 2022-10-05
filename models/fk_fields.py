b84db3fb919ebe8681dd2b5a187fa0ea5e53ef0a
Jonathan Tsai <hello@jontsai.com>
2022-10-05 18:39:52 +0000

    - allows fk_fields.build_kwargs to have **kwargs passed in - adds fk_organization

--- models/fk_fields.py
+++ models/fk_fields.py
@@ -1,3 +1,6 @@
+# Python Standard Library Imports
+import copy
+
 # Django Imports
 from django.conf import settings
 from django.db import models
@@ -21,19 +24,25 @@ NOT_REQUIRED_KWARGS = {
 }
 
 
-def build_kwargs(required: bool = False) -> dict:
-    kwargs = REQUIRED_KWARGS if required else NOT_REQUIRED_KWARGS
-    return kwargs
+def build_kwargs(required: bool = False, **kwargs) -> dict:
+    combined_kwargs = copy.copy(
+        REQUIRED_KWARGS if required else NOT_REQUIRED_KWARGS
+    )
+    for k, v in kwargs.items():
+        combined_kwargs[k] = v
+    return combined_kwargs
 
 
 ##
 # Users
 
 
-def fk_user(related_name: str, required: bool = False) -> models.ForeignKey:
+def fk_user(
+    related_name: str, required: bool = False, **kwargs
+) -> models.ForeignKey:
     field = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name=related_name,
-        **build_kwargs(required=required),
+        **build_kwargs(required=required, **kwargs),
     )
     return field
