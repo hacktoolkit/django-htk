d53581f3d89b2b8befe589626e7aed2f6c50f952
Jonathan Tsai <jontsai@users.noreply.github.com>
2022-10-29 19:32:33 +0000

    allow early_access_code field to be blank

--- apps/prelaunch/models.py
+++ apps/prelaunch/models.py
@@ -19,13 +19,24 @@ from htk.utils.request import get_current_request
 
 # isort: off
 
+
 class PrelaunchSignup(models.Model):
-    site = models.ForeignKey(Site, on_delete=models.CASCADE, null=True, blank=True)
+    site = models.ForeignKey(
+        Site,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
     first_name = models.CharField(max_length=64, blank=True)
     last_name = models.CharField(max_length=64, blank=True)
     email = models.EmailField(null=True, blank=True)
     early_access = models.BooleanField(default=False)
-    early_access_code = models.CharField(max_length=64, unique=True, null=True)
+    early_access_code = models.CharField(
+        max_length=64,
+        unique=True,
+        null=True,
+        blank=True,
+    )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -34,26 +45,32 @@ class PrelaunchSignup(models.Model):
         verbose_name = 'Prelaunch Signup'
 
     def __str__(self):
-        s = '%s - %s' % (self.created_on, self.email,)
+        s = '%s - %s' % (
+            self.created_on,
+            self.email,
+        )
         return s
 
     @property
     def full_name(self):
-        separator = ' ' if self.first_name.strip() and self.last_name.strip() else ''
+        separator = (
+            ' ' if self.first_name.strip() and self.last_name.strip() else ''
+        )
         full_name = '{}{}{}'.format(
             self.first_name.strip(),
             separator,
-            self.last_name.strip()
+            self.last_name.strip(),
         )
         return full_name
 
     def send_notifications(self):
         if htk_setting('HTK_SLACK_NOTIFICATIONS_ENABLED'):
             from htk.utils.notifications import slack_notify
+
             try:
                 message = '{} <{}> just signed up for the pre-launch waiting list.'.format(
                     self.full_name,
-                    self.email
+                    self.email,
                 )
                 slack_notify(message)
             except:
