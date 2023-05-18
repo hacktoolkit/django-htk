4ca8565b3fc6650712c7157ab6998c34b78beeb7
goztrk <gokhan@eg-ozturk.com>
2023-05-18 15:07:21 +0000

    Add base files

---
+++ apps/changelog/urls.py
@@ -0,0 +1,10 @@
+# Django Imports
+from django.conf.urls import url
+
+# HTK Imports
+import htk.apps.changelog.views as views
+
+
+urlpatterns = [
+    # url(r'^$', views.changelog_view, name='htk_apps_changelog'),
+]
