9294d1847233674e52da444e5d7d07fbd372d744
goztrk <gokhan@eg-ozturk.com>
2023-05-23 18:14:37 +0000

    Update ChangeLog class

--- apps/changelog/classes/change_log.py
+++ apps/changelog/classes/change_log.py
@@ -1,33 +1,32 @@
-# HTK Imports
-from htk.apps.changelog.constants.defaults import (
-    HTK_CHANGELOG_SLACK_CHANNEL_RELEASES,
-    HTK_CHANGELOG_SLACK_FULL_LOG_MESSAGE,
-)
+# Python Standard Library Imports
+from collections import namedtuple
 
 
 # isort: off
 
 
-class ChangeLog:
-    def __init__(
-        self,
-        origin_url,
-        log_entries,
-        slack_announce=False,
-        slack_channel=HTK_CHANGELOG_SLACK_CHANNEL_RELEASES,
-        slack_full_log_message=HTK_CHANGELOG_SLACK_FULL_LOG_MESSAGE,
-    ):
-        self.origin_url = origin_url
-        self.log_entries = log_entries
-        self.slack = {
-            'announce': slack_announce,
-            'channel': slack_channel,
-            'full_log_message': slack_full_log_message,
-        }
+class ChangeLog(
+    namedtuple(
+        'ChangeLog',
+        'origin_url,log_entries',
+    )
+):
+    # origin_url: str
+    # log_entries: list[LogEntry]
 
-    def write_changelog(self, changelog_file_name='CHANGELOG.md'):
+    def write_changelog(
+        self,
+        changelog_file_name='CHANGELOG.md',
+        slack_announce=False,
+        slack_channel='#releases',
+        web_url=None,
+    ):
+        """Write Change Log to given file.
+
+        It can announce the next release to slack.
+        """
         buf = []
-        slack_buf = [] if self.slack['announce'] else None
+        slack_buf = [] if slack_announce else None
         most_recent_release = True
 
         for i, log_entry in enumerate(self.log_entries):
@@ -35,7 +34,7 @@ class ChangeLog:
             if i == 0 and not log_entry.is_release:
                 buf.append('# Next Release')
                 buf.append('')
-                if self.slack['announce']:
+                if slack_announce:
                     slack_buf.append('*Most Recent Release*')
                     slack_buf.append('')
             elif log_entry.is_release:
@@ -56,7 +55,7 @@ class ChangeLog:
 
             # write log entry
             buf.append(log_entry.html)
-            if self.slack['announce'] and most_recent_release:
+            if slack_announce and most_recent_release:
                 slack_buf.append(log_entry.slack_message)
 
         # adds terminal newline
@@ -65,15 +64,19 @@ class ChangeLog:
         with open(changelog_file_name, 'w') as f:
             f.write('\n'.join(buf))
 
-        if self.slack['announce']:
+        if slack_announce:
             from htk.lib.slack.utils import webhook_call as slack_webhook_call
 
-            slack_buf.append('')
-            slack_buf.append(self.slack['full_log_message'])
+            if web_url is not None:
+                slack_buf.append('')
+                slack_buf.append(
+                    'The full log can be found at: {}'.format(web_url)
+                )
+
             slack_message = '\n'.join(slack_buf)
 
             slack_webhook_call(
-                channel=self.slack['channel'],
+                channel=slack_channel,
                 text=slack_message,
                 unfurl_links=False,
             )
