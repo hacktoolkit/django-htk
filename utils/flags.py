53e95ad7c583f8c5a3ad0c33a9082a32b4a4d3f4
goztrk <me@gokhan.org.tr>
2023-06-19 12:53:51 +0300

    Add flag enum utils

---
+++ utils/flags.py
@@ -0,0 +1,26 @@
+"""
+Flag Enum
+
+Usable in Python >= 3.6
+
+There is no backport for Python 2.7
+
+`auto` is imported to easily import from same file
+Ex: `from htk.utils.flags import HtkIntFlag, auto`
+"""
+from enum import IntFlag, auto  # noqa: F401
+
+
+class HtkIntFlag(IntFlag):
+    """HTK Int Flag Enum
+
+    Provides extra functionalities to Python's built-in IntFlag enum type
+    """
+    @classmethod
+    def extract_flags(cls, flags):
+        """Extract Flags
+
+        Returns: list of the bitwise or'ed combination.
+        """
+        flag_list = [flag for flag in flags if cls & flag > 0]
+        return flag_list
