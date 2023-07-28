13e0ac457bf78663f82109fb5f8722f5531314d5
mbruckhaus <mbruckhaus@talentral.com>
2023-07-28 18:31:07 +0000

    Get queue position & creation time

--- decorators/celery_.py
+++ decorators/celery_.py
@@ -1,5 +1,6 @@
 # Python Standard Library Imports
 from functools import wraps
+from datetime import datetime
 
 # Third Party (PyPI) Imports
 import rollbar
@@ -10,11 +11,21 @@ from htk.utils import htk_setting
 from htk.utils.notifications import slack_notify
 from htk.utils.timer import HtkTimer
 
+# Zippy Imports
+from zippy.celeryconfig import app as celery_app
+
 
 class safe_timed_task(object):
     def __init__(self, task_name, notify=False):
         self.task_name = task_name
         self.notify = notify
+        self.time_created = datetime.now()
+
+        with celery_app.connection() as connection:
+            queue = connection.default_channel.queue_declare(queue='zippy_prod_celery', passive=True)
+            queue_position = queue.message_count + 1
+        
+        self.queue_position = queue_position
 
     def __call__(self, task_fn):
         @shared_task
@@ -24,6 +35,7 @@ class safe_timed_task(object):
                 slack_notifications_enabled = self.notify and htk_setting('HTK_SLACK_NOTIFICATIONS_ENABLED')
                 if slack_notifications_enabled:
                     slack_notify('Processing *%s*...' % self.task_name)
+                print('Processing %s...' % self.task_name)
 
                 timer = HtkTimer()
                 timer.start()
@@ -33,7 +45,7 @@ class safe_timed_task(object):
                 if slack_notifications_enabled:
                     duration = timer.duration()
                     msg = 'Finished processing *%s* in *%s* seconds' % (self.task_name, duration,)
-                    slack_notify(msg)
+                    print(msg)
             except:
                 result = None
                 extra_data = {
