024c217089cc6254883faec48a8ee768f9a6d45c
Jonathan Tsai <hello@jontsai.com>
2024-01-15 22:27:31 +0000

    improve test scaffold utils

--- test_scaffold/utils.py
+++ test_scaffold/utils.py
@@ -9,48 +9,77 @@ from htk.compat import uuid4_hex
 from htk.test_scaffold.test_data import *
 
 
-def create_test_user():
+def create_test_user(
+    username=None,
+    first_name='',
+    last_name='',
+    email='',
+):
     """Creates a new user with random username for testing
     If two randomly assigned usernames overlap, it will fail
     """
     UserModel = get_user_model()
-    username = '%s_%s' % ('test', uuid4_hex()[:10],)
-    user = UserModel.objects.create(username=username)
+
+    if username is None:
+        username = create_test_username()
+
+    user = UserModel.objects.create(
+        username=username,
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+    )
+
     return user
 
-def create_test_email():
-    email = 'test%s@%s' % (
+
+def create_test_email(email_domain='hacktoolkit.com'):
+    email = 'test{}@{}'.format(
         uuid4_hex()[:10],
-        'hacktoolkit.com',
+        email_domain,
     )
     return email
 
+
 def create_test_username():
-    """Generates a random username
-    """
-    username = '%s_%s' % ('test', uuid4_hex()[:10],)
+    """Generates a random username"""
+    username = '{}_{}'.format(
+        'test',
+        uuid4_hex()[:10],
+    )
     return username
+
 
 def create_test_password():
     password = uuid4_hex()
     return password
 
+
 def create_test_user_with_email_and_password():
-    user = create_test_user()
     email = create_test_email()
+    user = create_test_user(email=email)
+
     password = create_test_password()
-    user.email = email
     user.set_password(password)
-    user.save()
-    return (user, email, password,)
+
+    return (
+        user,
+        email,
+        password,
+    )
+
 
 def get_test_display_name():
-    display_name = random.choice(TEST_DISPLAY_NAMES) + str(random.randint(1, 1000000))
+    display_name = random.choice(TEST_DISPLAY_NAMES) + str(
+        random.randint(1, 1000000)
+    )
     return display_name
+
 
 def get_test_username():
     username = random.choice(TEST_USERNAMES) + str(random.randint(1, 1000000))
     return username
+
 
 def get_random_string(max_length=0):
     s = 'randstr%s' % uuid4_hex()
