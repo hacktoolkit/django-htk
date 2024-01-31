2b5847e32b8e7d245732b9dc16e49e5cc0de075a
goztrk <me@gokhan.org.tr>
2024-02-01 00:46:11 +0300

    fix: 🐛 HttpErrorResponseMiddleware

--- middleware/classes.py
+++ middleware/classes.py
@@ -136,6 +136,13 @@ class HttpErrorResponseMiddleware:
     instead.
     """
 
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
     def process_exception(self, request, exception):
         if isinstance(exception, HttpErrorResponseError):
             response = exception.response
