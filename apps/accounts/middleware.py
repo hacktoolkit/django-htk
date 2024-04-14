24d33420d8bfe6a20fb90231b52b9d346b101676
Jonathan Tsai <jontsai@users.noreply.github.com>
2024-04-14 23:27:59 +0000

    adds HTTP Basic Auth middleware, improvements to Token auth

--- apps/accounts/middleware.py
+++ apps/accounts/middleware.py
@@ -1,5 +1,3 @@
-# Python Standard Library Imports
-
 # Django Imports
 from django.contrib.auth import (
     authenticate,
@@ -11,14 +9,130 @@ from django.utils.deprecation import MiddlewareMixin
 from social_django.middleware import SocialAuthExceptionMiddleware
 
 # HTK Imports
+from htk.api.utils import json_response_forbidden
 from htk.apps.accounts.utils.auth import login_authenticated_user
+from htk.apps.accounts.utils.general import authenticate_user_by_basic_auth_credentials
 from htk.utils import htk_setting
 
 
 # isort: off
 
 
-class HtkUserTokenAuthMiddleware(MiddlewareMixin):
+class BaseHtkAuthMiddleware(MiddlewareMixin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Instance attribute to determine whether in explicit authorization flow
+        self.is_explicit_auth = False
+
+    def _handle_auth_flow(self, request, auth_user=None):
+        """Performs authentication flow with `auth_user`
+
+        Helper function for `self.process_request`
+
+        Checks for any security violations and proceeds to log in `auth_user`
+        if everything is kosher.
+
+        The following security violations are checked:
+        - Explicit authorization requested, but invalid token user
+        - Implicit/explicit authorization, but mismatching users
+
+        Side effects:
+        - Logs in `auth_user` if flow is valid
+        - Logs out of current session if flow is detected to be invalid
+        """
+        already_logged_in_user = (
+            request.user
+            if hasattr(request, 'user') and request.user.is_authenticated
+            else None
+        )
+
+        if already_logged_in_user:
+            # there was already a logged in user.
+            if (
+                # explicit authorization requested, invalid `auth_user`
+                (self.is_explicit_auth and auth_user is None)
+                # (im/ex)plicit authorization requested, but mismatching users
+                or (
+                    auth_user is not None
+                    and auth_user != already_logged_in_user
+                )
+            ):
+                request.user = None
+                logout(request)
+                # TODO: redirect user or display error?
+                pass
+            else:
+                # no security violations
+                pass
+
+        elif auth_user:
+            # token user not logged in yet
+            login_authenticated_user(request, auth_user)
+            request.user = auth_user
+
+        else:
+            # no valid `auth_user` found, do nothing
+            # `self.process_response` will determine whether request should be
+            # processed regularly, or denied
+            pass
+
+    def process_response(self, request, response):
+        """Checks whether in an explicit authorization flow
+
+        Allows request to process normally if authorization succeeded,
+        or deny request if a security violation/anomaly was detected
+        """
+        if self.is_explicit_auth and not hasattr(request, 'user'):
+            if request.content_type == 'application/json':
+                response = json_response_forbidden()
+            else:
+                response = HttpResponse('Forbidden', status=403)
+
+        return response
+
+
+class HtkBasicAuthMiddleware(BaseHtkAuthMiddleware):
+    """Custom Authenication Middleware to allow logging in using
+    Basic authorization in HTTP headers.
+    """
+
+    def process_request(self, request):
+        auth_user = None
+
+        if 'HTTP_AUTHORIZATION' in request.META:
+            self.is_explicit_auth = True
+            auth_header = request.META['HTTP_AUTHORIZATION']
+            parts = auth_header.split()
+            if len(parts) == 2:
+                token_type, credentials = parts
+                if token_type == 'Basic':
+                    # only Basic auth can continue
+
+                    auth_user = authenticate_user_by_basic_auth_credentials(
+                        request, credentials
+                    )
+
+                elif token_type == 'Bearer':
+                    # Do not count Bearer auth as explicit auth,
+                    # to give a chance for `HttpUserTokenAuthMiddleware` to work
+
+                    self.is_explicit_auth = False
+
+                else:
+                    # unsupported `token_type`
+                    pass
+
+            else:
+                # unexpected authorization value
+                pass
+        else:
+            pass
+
+        self._handle_auth_flow(request, auth_user)
+
+
+class HtkUserTokenAuthMiddleware(BaseHtkAuthMiddleware):
     """Custom Authentication Middleware to attempt logging in with a securely generated token
 
     Tokens are checked in the following order:
@@ -31,49 +145,39 @@ class HtkUserTokenAuthMiddleware(MiddlewareMixin):
     """
 
     def process_request(self, request):
-        already_logged_in_user = (
-            request.user
-            if hasattr(request, 'user') and request.user.is_authenticated
-            else None
-        )
-
         if 'HTTP_AUTHORIZATION' in request.META:
-            # must use `Bearer` token for now
+            self.is_explicit_auth = True
             auth_header = request.META['HTTP_AUTHORIZATION']
             parts = auth_header.split()
             if len(parts) == 2:
-                method, token = parts
-                if method != 'Bearer':
-                    token = None
-                else:
+                token_type, token = parts
+                if token_type == 'Bearer':
+                    # Only Bearer token can continue
                     pass
+                else:
+                    # unset token
+                    token = None
+
+                    if token_type == 'Basic':
+                        # Do not count Basic auth as explicit auth,
+                        # to give a chance for `HtkBasicAuthMiddleware` to work
+                        self.is_explicit_auth = False
+                    else:
+                        pass
             else:
+                # unexpected authorization value
                 token = None
         elif 'token' in request.GET:
+            # implicit authenication token using URL params
             token = request.GET['token']
         else:
+            # No authentication token provided via available methods
             token = None
 
-        token_user = (
+        auth_user = (
             authenticate(request=request, token=token) if token else None
         )
-
-        if token_user:
-            if already_logged_in_user:
-                if token_user != already_logged_in_user:
-                    # mismatch between logged-in user and token user
-                    logout(request)
-                    # TODO: redirect user or display error?
-                else:
-                    # matches, do nothing
-                    pass
-            else:
-                # token user not logged in yet
-                login_authenticated_user(request, token_user)
-                request.user = token_user
-        else:
-            # no valid user found from token, do nothing
-            pass
+        self._handle_auth_flow(request, auth_user)
 
 
 class HtkSocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
