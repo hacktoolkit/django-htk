b9b05e67c21a61115bc89d23d3c20a3d9f768b98
Shreyas Telkar <shretel@gmail.com>
2024-07-02 21:28:23 +0000

    Modifies notifications. Changes to `JoinRequest` model.

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -395,11 +395,7 @@ class BaseAbstractOrganizationJoinRequest(HtkBaseModel):
         return msg
 
     def build_notification_message__created(self):
-        msg = self._build_notification_message(self.invited_by, 'sent')
-        return msg
-
-    def build_notification_message__resent(self):
-        msg = self._build_notification_message(self.invited_by, 're-sent')
+        msg = self._build_notification_message(self.user, 'sent')
         return msg
 
     def build_notification_message__accepted(self):
