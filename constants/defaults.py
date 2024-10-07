4340edfcf8f3732b5d73e5f28b4314090c59f4b9
shreyastelkar <shretel@gmail.com>
2024-10-07 17:48:15 +0000

    Adds file blob max size to constants

--- constants/defaults.py
+++ constants/defaults.py
@@ -93,6 +93,7 @@ HTK_JS_ROUTES_DYNAMIC_PART_PLACEHOLDER = '{0}'
 from htk.admintools.constants.defaults import *
 from htk.apps.accounts.constants.defaults import *
 from htk.apps.bible.constants.defaults import *
+from htk.apps.blob_storage.constants.defaults import *
 from htk.apps.changelog.constants.defaults import *
 from htk.apps.conversations.constants.defaults import *
 from htk.apps.cpq.constants.defaults import *
