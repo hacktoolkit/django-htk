b34117c9c10954fea195c4ec60c896c32ca1e197
Jonathan Tsai <jontsai@users.noreply.github.com>
2025-02-05 09:31:16 +0000

    Adds check for `user.is_authenticated`

--- admintools/decorators.py
+++ admintools/decorators.py
@@ -19,8 +19,8 @@ def company_employee_required(view_func):
     """
     @login_required
     def wrapped_view(request, *args, **kwargs):
-        user = request.user
-        if not(user.profile and user.profile.is_company_employee):
+        user = request.user if request.user.is_authenticated else None
+        if not(user and user.profile and user.profile.is_company_employee):
             raise PermissionDenied
         return view_func(request, *args, **kwargs)
     return wrapped_view
