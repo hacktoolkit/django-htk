4118400b97a12830cf2d91a1b72f0292e91f8ee4
Jonathan Tsai <jontsai@users.noreply.github.com>
2025-02-05 09:48:04 +0000

    refactor `request.user` check, provide util for `_extract_request_user`

--- admintools/decorators.py
+++ admintools/decorators.py
@@ -3,24 +3,34 @@ from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 
 
+def _extract_request_user(request):
+    user = (
+        request.user if request.user and request.user.is_authenticated else None
+    )
+    return user
+
+
 def company_officer_required(view_func):
-    """Decorator for views that require access by company officer or staff user
-    """
+    """Decorator for views that require access by company officer or staff user"""
+
     @login_required
     def wrapped_view(request, *args, **kwargs):
-        user = request.user
-        if not(user.profile and user.profile.is_company_officer):
+        user = _extract_request_user(request)
+        if not (user and user.profile and user.profile.is_company_officer):
             raise PermissionDenied
         return view_func(request, *args, **kwargs)
+
     return wrapped_view
 
+
 def company_employee_required(view_func):
-    """Decorator for views that require access by company employee or staff user
-    """
+    """Decorator for views that require access by company employee or staff user"""
+
     @login_required
     def wrapped_view(request, *args, **kwargs):
-        user = request.user if request.user.is_authenticated else None
-        if not(user and user.profile and user.profile.is_company_employee):
+        user = _extract_request_user(request)
+        if not (user and user.profile and user.profile.is_company_employee):
             raise PermissionDenied
         return view_func(request, *args, **kwargs)
+
     return wrapped_view
