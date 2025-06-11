0a5dd8a538694f979974aa70c3569c365ac9ac9f
Yosef Ashenafi <jossyfreelancer@gmail.com>
2025-06-11 21:56:47 +0300

    fix 🐛 Update import for InvitationStatus (#488)
    - Update import for the InvitationStatus from apps invitation status
    instead of error throwing mesh.

--- apps/invitations/utils.py
+++ apps/invitations/utils.py
@@ -5,6 +5,6 @@ from htk.utils.enums import get_enum_choices
 
 
 def get_invitation_status_choices():
-    from mesh.enums import InvitationStatus
+    from htk.apps.invitations.enums import InvitationStatus
     choices = get_enum_choices(InvitationStatus)
     return choices
